ad38afb92aebc93e3c654caf1644fab9c569e648
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-12-30 17:20:33 +0100

    Explain '@StateObject' vs '@ObservedObject'

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 ## Text example
 
 ```swift
+struct PersistentCounterView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    var body: some View {
+        ...
+    }
+}
+
+struct VolatileCounterView: View {
+    
+    @ObservedObject private var viewModel = ContentViewModel()
+    var body: some View {
+        ...
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject private var viewModel = ContentViewModel()
+    @State var randomNumber = 0
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Active: \(randomNumber)")
+            Button {
+                randomNumber = (0..<1000).randomElement() ?? 0
+            } label: {
+                Text("Reset view")
+            }
+            PersistentCounterView()
+            VolatileCounterView()
+        }
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,75 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var counter: Int
+    
+    init() {
+        counter = 0
+    }
+    
+    func increment() {
+        counter += 1
+    }
+}
+
+struct PersistentCounterView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    var body: some View {
+        VStack {
+            Text("This is persistent counter - @StateObject")
+            HStack {
+                Button {
+                    viewModel.increment()
+                } label: {
+                    Text("Increment")
+                }
+                Text("\(viewModel.counter)")
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(.green)
+    }
+}
+
+struct VolatileCounterView: View {
+    
+    @ObservedObject private var viewModel = ContentViewModel()
+    var body: some View {
+        VStack {
+            Text("This is volatile counter - @ObservedObject")
+            HStack {
+                Button {
+                    viewModel.increment()
+                } label: {
+                    Text("Increment")
+                }
+                Text("\(viewModel.counter)")
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(.orange)
+    }
+}
 
 struct ContentView: View {
+    @State var randomNumber = 0
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Active: \(randomNumber)")
+            Button {
+                randomNumber = (0..<1000).randomElement() ?? 0
+            } label: {
+                Text("Reset view")
+            }
+            PersistentCounterView()
+            VolatileCounterView()
+        }
     }
 }
 
