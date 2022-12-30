678bf2516a5979d385357436295ab8b799d3ae61
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-12-30 10:21:59 +0100

    Add sample README and preview

---
+++ README.md
@@ -0,0 +1,8 @@
+## Positioning image
+
+- `scaledToFit` - image is scaled to match the smallest dimension of its frame
+- `scaledToFill` - image is scaled to match the biggest dimension of its frame
+
+- `aspectRation` - works as `scaledToFit` or `scaledToFill` but also allows controlling how much a view is stretched or filled.
+
+<img src="preview.png" width="60%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -11,7 +11,92 @@ import CoreData
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            VStack {
+                Text("frame(width: 60, height: 30)")
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .frame(width: 60, height: 30)
+                    .background(.red)
+            }.padding(.bottom, 10)
+            VStack {
+                Text("scaledToFit")
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 30)
+                    .background(.red)
+            }.padding(.bottom, 10)
+            VStack {
+                Text("scaledToFill")
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 30)
+                    .background(.red)
+            }.padding(.bottom, 10)
+
+            VStack {
+                VStack {
+                    Text("aspectRatio(1.5, contentMode: .fit)")
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .aspectRatio(1.5, contentMode: .fit)
+                        .frame(width: 60, height: 30)
+                        .background(.red)
+                }.padding(.bottom, 10)
+                
+                VStack {
+                    Text("aspectRatio(1.0, contentMode: .fit)")
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .frame(width: 60, height: 30)
+                        .background(.red)
+                }.padding(.bottom, 10)
+                
+                VStack {
+                    Text("aspectRatio(0.5, contentMode: .fit)")
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .aspectRatio(0.5, contentMode: .fit)
+                        .frame(width: 60, height: 30)
+                        .background(.red)
+                }.padding(.bottom, 10)
+            }.background(.yellow)
+            
+            VStack {
+                VStack {
+                    Text("aspectRatio(1.5, contentMode: .fill)")
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .aspectRatio(1.5, contentMode: .fill)
+                        .frame(width: 60, height: 30)
+                        .background(.red)
+                }.padding(.bottom, 10)
+                
+                VStack {
+                    Text("aspectRatio(1.0, contentMode: .fill)")
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .aspectRatio(1.0, contentMode: .fill)
+                        .frame(width: 60, height: 30)
+                        .background(.red)
+                }.padding(.bottom, 10)
+
+                VStack {
+                    Text("aspectRatio(0.5, contentMode: .fill)")
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .aspectRatio(0.5, contentMode: .fill)
+                        .frame(width: 60, height: 30)
+                        .background(.red)
+                }.padding(.bottom, 10)
+            }.background(.orange)
+
+
+            
+        }
     }
 }
 
