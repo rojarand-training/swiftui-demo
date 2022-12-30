b7a1ef94e54da028ca3ca4e91ae63e518adf267e
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-12-30 11:54:28 +0100

    Add Binding usage example

--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -10,8 +10,31 @@ import CoreData
 
 struct ContentView: View {
 
+    @State var text: String = "Text"
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                Text("I'm a parent view")
+                    .foregroundColor(.red)
+                Text(text)
+                //We can retrieve binding (projected value of a state) by prefixing the property variable with a dollar sign (`$`)
+                NavigationLink(destination: EditingView(text: $text)) {
+                    Text("Click to edit text")
+                }
+            }
+        }
+    }
+}
+
+struct EditingView: View {
+    @Binding var text: String
+    var body: some View {
+        VStack {
+            Text("I'm a child view")
+                    .foregroundColor(.red)
+            TextField("Edit me", text: $text)
+                .frame(width: 150)
+        }
     }
 }
 
