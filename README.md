8430935ebad553817a2a5e0f91b3e0b8db03215b
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-01 11:50:46 +0100

    Add Combine+zip example

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
-## Text example
+## Combine zip example
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+final class ContentViewModel: ObservableObject {
+    private var cancellableSet: Set<AnyCancellable> = []
+    @Published var randomValue: String = ""
+    
+    private var integerGenerator = PassthroughSubject<Int, Never>()
+    private var stringGenerator = PassthroughSubject<String, Never>()
+    
+    init() {
+        
+        let delayedStringGenerator = stringGenerator
+            .delay(for: 1, scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+        
+        integerGenerator.zip(delayedStringGenerator)
+            .sink { (generatedInteger, generatedString) in
+                self.randomValue = "\(generatedInteger) - \(generatedString)"
+            }
+            .store(in: &cancellableSet)
+    }
+    
+    func generate() {
+        integerGenerator.send(generateInteger())
+        stringGenerator.send("\(generateInteger())")
+    }
+    
+    private func generateInteger() -> Int {
+        return (1...1000).randomElement() ?? 0
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,53 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+final class ContentViewModel: ObservableObject {
+    private var cancellableSet: Set<AnyCancellable> = []
+    @Published var randomValue: String = ""
+    
+    private var integerGenerator = PassthroughSubject<Int, Never>()
+    private var stringGenerator = PassthroughSubject<String, Never>()
+    
+    init() {
+        
+        let delayedStringGenerator = stringGenerator
+            .delay(for: 1, scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+        
+        integerGenerator.zip(delayedStringGenerator)
+            .sink { (generatedInteger, generatedString) in
+                self.randomValue = "\(generatedInteger) - \(generatedString)"
+            }
+            .store(in: &cancellableSet)
+    }
+    
+    func generate() {
+        integerGenerator.send(generateInteger())
+        stringGenerator.send("\(generateInteger())")
+    }
+    
+    private func generateInteger() -> Int {
+        return (1...1000).randomElement() ?? 0
+    }
+}
+
 
 struct ContentView: View {
 
+    @ObservedObject
+    private var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Random value: \(viewModel.randomValue)")
+            Button {
+                viewModel.generate()
+            } label: {
+                Text("Generate")
+            }
+        }
     }
 }
 
