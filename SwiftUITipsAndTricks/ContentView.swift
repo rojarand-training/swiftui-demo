e4d234aa0f63c9f5b3144e5879022733a1c8ad1e
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-02 11:24:16 +0100

    Add banner animation

--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-
+struct BannerView: View {
+    let message: String
+    var show: Bool
+    
     var body: some View {
-        Text("Hello World")
+        Text(message)
+            .font(.title)
+            .frame(width:UIScreen.main.bounds.width - 20,
+                   height: 100)
+            .foregroundColor(.white)
+            .background(Color.green)
+            .cornerRadius(10)
+            .offset(y: show ?
+                    UIScreen.main.bounds.height / 2.5 :
+                        UIScreen.main.bounds.height)
+            .animation(
+                .interpolatingSpring(mass: 1.0,
+                                     stiffness: 100.0,
+                                     damping: 10,
+                                     initialVelocity: 0),
+                value: show)
+    }
+}
+
+struct ContentView: View {
+    @State
+    var show = false
+    
+    var body: some View {
+        VStack {
+            BannerView(message: "Hello, World!", show: show)
+            Button {
+                show.toggle()
+            } label: {
+                Text(show ? "Hide" : "Show")
+                    .padding()
+                    .frame(width: 100)
+                    .foregroundColor(.white)
+                    .background(show ? .red : .blue)
+                    .cornerRadius(10)
+            }
+        }
     }
 }
 
