ca7a5ca3ade3105506d29f78635bd332e0372d91
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-05 09:27:09 +0100

    Explain @ViewBuilder attribute

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-## Text example
+## `@ViewBuilder` example
+
+The `@ViewBuilder` attribute is one of the few **result builders** in SwiftUI. You typically use it to create child views for a specific SwiftUI view in a readable way **without having to use any return keywords**. It makes you able to anotate functions too.
+
+
 
 ```swift
-struct ContentView: View {
+/*
+Error: Function declares an opaque return type 'some View', but the return statements in its body do not have matching underlying types
+*/
+func sampleView(aCondition: Bool) -> some View {
+    if aCondition {
+        return Text("A condition fullfiled")
+    } else {
+        return EmptyView()
+    }
+}
 
-    var body: some View {
-        Text("Hello World")
+//This version compiles. Note there are no "returns".
+@ViewBuilder
+func sampleView(aCondition: Bool) -> some View {
+    if aCondition {
+        Text("A text created by a function")
+    } else {
+        EmptyView()
     }
 }
 ```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,113 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-
+struct CircleWithoutViewBuilder<Content: View>: View {
+    var content: () -> Content
+    
+    init(content: @escaping () -> Content) {
+        self.content = content
+    }
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Circle().foregroundColor(.blue)
+            content()
+        }
+    }
+}
+
+struct CircleWithViewBuilder<Content: View>: View {
+    var content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        ZStack {
+            Circle().foregroundColor(.red)
+            content()
+        }
+    }
+}
+
+struct CircleWithInlinedViewBuilder<Content: View>: View {
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        ZStack {
+            Circle().foregroundColor(.red)
+            content()
+        }
+    }
+}
+
+
+struct ContentView: View {
+    var condition: Bool = true
+    
+    /*
+     Error: Function declares an opaque return type 'some View', but the return statements in its body do not have matching underlying types
+    func sampleView(aCondition: Bool) -> some View {
+        if aCondition {
+            return Text("A condition fullfiled")
+        } else {
+            return EmptyView()
+        }
+    }*/
+    
+    @ViewBuilder
+    func sampleView(aCondition: Bool) -> some View {
+        if aCondition {
+            Text("A text created by a function")
+        } else {
+            EmptyView()
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            
+            sampleView(aCondition: true)
+            
+            CircleWithoutViewBuilder {
+                Text("I'm compiling")
+            }
+            /* Multiple nested views
+             Error: Type '()' cannot conform to 'View'
+             CircleWithoutViewBuilder {
+                Text("I'm first view")
+                Text("I'm second view")
+            }*/
+            
+            /* Conditions
+             Error: Type '()' cannot conform to 'View'
+            CircleWithoutViewBuilder {
+                if condition {
+                    Text("I'm compilling")
+                } else {
+                    Color(.blue)
+                }
+            }*/
+           
+            //Multiple nested views
+            CircleWithViewBuilder {
+                Rectangle()
+                    .frame(width: 150, height: 50)
+                    .foregroundColor(.gray)
+                Text("I'm a text view")
+            }
+            
+            //Multiple nested views
+            CircleWithInlinedViewBuilder {
+                if condition {
+                    Text("I'm a text view")
+                } else {
+                    EmptyView()
+                }
+            }
+            
+        }
     }
 }
 
