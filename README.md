260de5addec3f8ff52f1f5d2d0f5614139f6b6a9
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-06 10:49:38 +0100

    Show how to execute computation in background

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
-## Text example
+## Combine working thread switching example
+
+>> Note!! `receive` must be under a computation otherwise, the computation takes place in in the same thread as receive points to.
 
 ```swift
-struct ContentView: View {
+//Wrong:
+.subscribe(on: DispatchQueue.global())
+.receive(on: DispatchQueue.main)
+.map { ... }
 
-    var body: some View {
-        Text("Hello World")
+//Good
+.subscribe(on: DispatchQueue.global())
+.map { ... }
+.receive(on: DispatchQueue.main)
+```
+
+```swift
+class NonBlockingRandomStringViewModel: ObservableObject {
+    @Published var content: String = ""
+    var cancellable: Cancellable?
+    
+    func regenerate() {
+        cancellable = Just(Int.random(in: 1...100))
+            .subscribe(on: DispatchQueue.global())
+            .map { (generated: Int) in
+                print("Inside map, main thread: \(Thread.isMainThread)")
+                Thread.sleep(forTimeInterval: 2)
+                return generated*2
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { generated in
+                print("Inside sink, main thread: \(Thread.isMainThread)")
+                self.content = "\(generated)"
+            }
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,82 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+class NonBlockingRandomStringViewModel: ObservableObject {
+    @Published var content: String = ""
+    var cancellable: Cancellable?
+    
+    func regenerate() {
+        cancellable = Just(Int.random(in: 1...100))
+            .subscribe(on: DispatchQueue.global())
+            .map { (generated: Int) in
+                print("Inside map, main thread: \(Thread.isMainThread)")
+                Thread.sleep(forTimeInterval: 2)
+                return generated*2
+            }
+            //Note receive must be under a computation otherwise,
+            //the computation takes place in in the same thread as receive points to.
+            .receive(on: DispatchQueue.main)
+            .sink { generated in
+                print("Inside sink, main thread: \(Thread.isMainThread)")
+                self.content = "\(generated)"
+            }
+    }
+}
+
+
+class BlockingRandomStringViewModel: ObservableObject {
+    var content: String = ""
+    var cancellable: Cancellable?
+    
+    func regenerate() {
+        cancellable = Just(Int.random(in: 1...100))
+            .map { (generated: Int) in
+                Thread.sleep(forTimeInterval: 2)
+                return generated*2
+            }
+            .sink { generated in
+                self.content = "\(generated)"
+                self.objectWillChange.send()
+            }
+    }
+}
+
+struct BlockingRandomStringView: View {
+    
+    @ObservedObject private var viewModel = BlockingRandomStringViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Content: \(viewModel.content)")
+            Button("Regenerate content - blocking") {
+                viewModel.regenerate()
+            }
+        }.background(.yellow)
+    }
+}
+
+struct NonBlockingRandomStringView: View {
+    
+    @ObservedObject private var viewModel = NonBlockingRandomStringViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Content: \(viewModel.content)")
+            Button("Regenerate content - non blocking") {
+                viewModel.regenerate()
+            }
+        }.background(.green)
+    }
+}
 
 struct ContentView: View {
-
     var body: some View {
-        Text("Hello World")
+        VStack {
+            BlockingRandomStringView()
+            NonBlockingRandomStringView()
+        }
     }
 }
 
