9a0e5ee75678bf8e788d0fc690f3bfb82f5f4233
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-08 08:43:52 +0100

    Give example of embedding UIKit inside SwiftUI

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-## Text example
+## Using UIViewControllers in SwiftUI
+
+> Note!!! To use `UIViewController` inside SwiftUI you must implement `UIViewControllerRepresentable` not `UIViewRepresentable`.
 
 ```swift
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        UIViewRepresentation()
+    }
+}
+
+struct UIViewRepresentation: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> SampleViewController {
+        return SampleViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: SampleViewController, context: Context) {
     }
 }
 ```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,17 +6,47 @@
 //
 
 import SwiftUI
-import CoreData
+import UIKit
 
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        UIViewRepresentation()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct UIViewRepresentation: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> SampleViewController {
+        return SampleViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: SampleViewController, context: Context) {
+    }
+}
+
+class SampleViewController: UIViewController {
+    
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello from UIViewController"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(infoLabel)
+        NSLayoutConstraint.activate(
+            [infoLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+             infoLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)]
+        )
+        self.view.backgroundColor = .orange
     }
 }
