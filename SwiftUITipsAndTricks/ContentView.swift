ee8f028450a9e77d70f20d42d1ddcd8419473680
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-11 08:20:09 +0100

    Add publisher receive notification example

--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+
+extension View {
+    
+    func onNotification(withName name: NSNotification.Name, perform action: @escaping () -> Void) -> some View {
+        return onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            action()
+        }
+    }
+    
+    func onWillEnterForeground(perform action: @escaping () -> Void) -> some View {
+        return onNotification(withName: UIApplication.willEnterForegroundNotification,
+                              perform: action)
+    }
+}
 
 struct ContentView: View {
 
+    @State var eventsLog = ""
     var body: some View {
-        Text("Hello World")
+        Text("Events:\n\(eventsLog)")
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification)) { _ in
+                eventsLog += "- Did enter background\n"
+            }
+            .onWillEnterForeground {
+                eventsLog += "- Will enter background\n"
+            }
     }
 }
 
