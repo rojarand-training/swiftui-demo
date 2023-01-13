da911203b7db0b6145bf27ac0835d6e9512e4abe
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-13 09:32:34 +0100

    Add example of advanced Combine extension function

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-## Text example
+## Advanced Combine convenience method
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+extension Publisher {
+    func unwrap<T, E: Error>(orThrow error: @escaping @autoclosure () -> E) -> Publishers.TryMap<Self, T>
+    where Output==Optional<T> {
+        return tryMap { output in
+            switch output {
+            case .some(let value):
+                return value
+            default:
+                throw error()
+            }
+        }
     }
+}
+
+func compute() {
+    cancellable = Just<Int?>( drawEvenNumber() )
+        .unwrap(orThrow: UnexpectedNilReceived())
+        .sink { result in
+            self.resultDescription = "Result: \(result)"
+        } receiveValue: { value in
+            self.resultValue = "\(value)"
+        }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,58 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+struct UnexpectedNilReceived: Error {}
+
+extension Publisher {
+    func unwrap<T, E: Error>(orThrow error: @escaping @autoclosure () -> E) -> Publishers.TryMap<Self, T>
+    where Output==Optional<T> {
+        return tryMap { output in
+            switch output {
+            case .some(let value):
+                return value
+            default:
+                throw error()
+            }
+        }
+    }
+}
+
+final class ContentViewModel: ObservableObject {
+    
+    @Published var resultValue: String = ""
+    @Published var resultDescription: String = ""
+    private var cancellable: Cancellable?
+    
+    private func drawEvenNumber() -> Int? {
+        let value = Int.random(in: 1...100)
+        return value%2 == 0 ? value : nil
+    }
+    
+    func compute() {
+        cancellable = Just<Int?>( drawEvenNumber() )
+            .unwrap(orThrow: UnexpectedNilReceived())
+            .sink { result in
+                self.resultDescription = "Result: \(result)"
+            } receiveValue: { value in
+                self.resultValue = "\(value)"
+            }
+    }
+}
 
 struct ContentView: View {
-
+    
+    @ObservedObject var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Result value: \(viewModel.resultValue)")
+            Text("Result description: \(viewModel.resultDescription)")
+            Button("Compute result ") {
+                viewModel.compute()
+            }
+        }
     }
 }
 
