4c4b4ccc900e9eba908179f9b948d2a91cecbba5
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-14 11:03:49 +0100

    Explain actors atomicity

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-## Text example
+## Actor's atomicity example
+
+Calling an actor's method must be done within `non-isolated context` (Task). Otherwise compilation finishes with error:
+```
+Actor-isolated instance method 'randomize()' can not be referenced from a non-isolated context
+```
+
+<img src="isolated-actor-access-error.png">
+
+Calling an actor's method requires the `await` keyword, despite the actor method is not denoted as `async`.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+actor ActorComputer {
+    func randomize() -> Int {
+        ...
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+Actors guarantee method excution atomicity. Check below.
+
+<img src="preview.gif" >
+
+> Note: Using Task/Task.detached changes the thread context to a global one (see logs above).
+
+Actors guarantee that mutable variables are updates in safe manner, because actor properties can be mutated only in an isolated context.
+
+<img src="nonisolated-mutation-error.png">
+
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,132 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+class ClassComputer {
+    
+    var currentValue = 0
+    
+    func double() -> Int {
+        print("--- Class/Before double, main thread: \(Thread.isMainThread)")
+        Thread.sleep(forTimeInterval: 2)
+        currentValue *= 2
+        print("Class/After double, main thread: \(Thread.isMainThread)")
+        return currentValue
+    }
+    
+    func randomize() -> Int {
+        print("--- Class/Before randomize, main thread: \(Thread.isMainThread)")
+        Thread.sleep(forTimeInterval: 2)
+        currentValue = Int.random(in: 1...1000)
+        print("Class/After randomize, main thread: \(Thread.isMainThread)")
+        return currentValue
+    }
+}
+
+actor ActorComputer {
+    
+    var currentValue = 0
+    
+    /*
+     //Compilation error:
+     //Actor-isolated property 'currentValue' can not be mutated from a non-isolated context
+    nonisolated func square() -> Int {
+        currentValue *= currentValue
+        return currentValue
+    }
+     */
+    
+    func double() -> Int {
+        print("--- Actor/Before double, main thread: \(Thread.isMainThread)")
+        Thread.sleep(forTimeInterval: 2)
+        currentValue *= 2
+        print("Actor/After double, main thread: \(Thread.isMainThread)")
+        return currentValue
+    }
+    
+    func randomize() -> Int {
+        print("--- Actor/Before randomize, main thread: \(Thread.isMainThread)")
+        Thread.sleep(forTimeInterval: 2)
+        currentValue = Int.random(in: 1...1000)
+        print("After randomize, main thread: \(Thread.isMainThread)")
+        return currentValue
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var classValue: Int = 0
+    @Published var actorValue: Int = 0
+    
+    private let classComputer = ClassComputer()
+    func classRandomize() {
+        Task.detached {
+            let value = self.classComputer.randomize()
+            await self.updateClassValue(newValue: value)
+        }
+    }
+    
+    func classDouble() {
+        Task.detached {
+            let value = self.classComputer.double()
+            await self.updateClassValue(newValue: value)
+        }
+    }
+    
+    @MainActor private func updateClassValue(newValue: Int) {
+        self.classValue = newValue
+    }
+    
+    private let actorComputer = ActorComputer()
+    func actorRandomize() {
+        Task.detached {
+            //Note actor do not use main thread any more
+            let value = await self.actorComputer.randomize()
+            await self.updateActorValue(newValue: value)
+        }
+    }
+    
+    func actorDouble() {
+        Task.detached {
+            let value = await self.actorComputer.double()
+            await self.updateActorValue(newValue: value)
+        }
+    }
+    
+    @MainActor private func updateActorValue(newValue: Int) {
+        self.actorValue = newValue
+    }
+}
+
+//TODO: print thread
 
 struct ContentView: View {
-
+    
+    @State var value: Int = 0
+    @ObservedObject var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        Spacer()
+        VStack {
+            Text("Current class value: \(viewModel.classValue)")
+            Button("Class - Randomize value") {
+                viewModel.classRandomize()
+            }.padding()
+            Button("Class - Double value") {
+                viewModel.classDouble()
+            }.padding()
+        }.background(.gray)
+
+        VStack {
+            Text("Current actor value: \(viewModel.actorValue)")
+            Button("Actor - Randomize value") {
+                viewModel.actorRandomize()
+            }.padding()
+            Button("Actor - Double value") {
+                viewModel.actorDouble()
+            }.padding()
+        }.background(.yellow)
     }
 }
 
