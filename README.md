0e1e0a40c626f4800f4675f23cfab51771b73668
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-18 10:51:30 +0100

    Show how to make failable Just

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
-## Text example
+## Combine Just/Faile example
+
+`.setFailureType(to: ErrorType)` allows to convert just to a failable publisher.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+class ContentViewModel: ObservableObject {
+    
+    func multiply(value: Int) -> AnyPublisher<Int, Error> {
+        guard value>0 else {
+            return Fail(error: PositiveIntRequired()).eraseToAnyPublisher()
+        }
+        /*Cannot convert return expression of type 'AnyPublisher<Int, Never>' to return type 'AnyPublisher<Int, any Error>'
+        return Just(value*2).eraseToAnyPublisher()
+         */
+        
+        return Just(value*2)
+            .setFailureType(to: Error.self) //we need to specify failure type
+            .eraseToAnyPublisher()
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,53 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+struct PositiveIntRequired: Error {
+}
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var receivedResult: String = ""
+    @Published var receivedValue: String = ""
+    
+    private var cancellable: Cancellable?
+    
+    func refresh() {
+        cancellable = multiply(value: Int.random(in: -1...2))
+            .sink { result in
+                self.receivedResult = "\(result)"
+            } receiveValue: { value in
+                self.receivedValue = "\(value)"
+            }
+    }
+    
+    func multiply(value: Int) -> AnyPublisher<Int, Error> {
+        guard value>0 else {
+            return Fail(error: PositiveIntRequired()).eraseToAnyPublisher()
+        }
+        /*Cannot convert return expression of type 'AnyPublisher<Int, Never>' to return type 'AnyPublisher<Int, any Error>'
+        return Just(value*2).eraseToAnyPublisher()
+         */
+        
+        return Just(value*2)
+            .setFailureType(to: Error.self) //we need to specify failure type
+            .eraseToAnyPublisher()
+    }
+}
 
 struct ContentView: View {
-
+    
+    @ObservedObject var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Result type: \(viewModel.receivedResult)")
+            Text("Result value: \(viewModel.receivedValue)")
+            Button("Refresh") {
+                viewModel.refresh()
+            }
+        }
     }
 }
 
