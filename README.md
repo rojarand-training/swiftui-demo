51619c39d2ee0aaa3281c65179aa5514c8f48e18
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-20 08:17:32 +0100

    Show example how to create custom env variable

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-## Text example
+## [Custom environment variable](https://useyourloaf.com/blog/swiftui-custom-environment-values/)
 
 ```swift
+// 1. Create the key with a default value
+struct CustomBackgroundColor: EnvironmentKey {
+    static var defaultValue: Color = .red
+}
+
+// 2. Extend the environment with our property
+extension EnvironmentValues {
+    var cbc: Color {
+        get { self[CustomBackgroundColor.self] }
+        set { self[CustomBackgroundColor.self] = newValue }
+    }
+}
+
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Parent(text: "No bg color defined")
+            Parent(text: "Bg color defined")
+                .environment(\.cbc, .blue)
+        }
     }
 }
 ```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 import CoreData
 
+struct CustomBackgroundColor: EnvironmentKey {
+    static var defaultValue: Color = .red
+}
+
+extension EnvironmentValues {
+    var cbc: Color {
+        get { self[CustomBackgroundColor.self] }
+        set { self[CustomBackgroundColor.self] = newValue }
+    }
+}
+
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Parent(text: "No bg color defined")
+            Parent(text: "Bg color defined")
+                .environment(\.cbc, .blue)
+        }
+    }
+}
+
+struct Child: View {
+    @Environment(\.cbc) var customColor: Color
+    
+    var body: some View {
+        Text("I'm a child view")
+            .background(customColor)
+    }
+}
+
+struct Parent: View {
+    let text: String
+    var body: some View {
+        HStack {
+            Text(text)
+            Child()
+        }
     }
 }
 
