088c4ba90cfbefbde7b7fd5d00f05d55bd790aea
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-21 11:17:01 +0100

    Show Hashable example

--- README.md
+++ README.md
@@ -1,12 +1,102 @@
-## Text example
+## [Hashable](https://developer.apple.com/documentation/swift/hashable)
+
+To make an object *hashable* you need to implement `Hashable` protocol for the object.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+struct P: Hashable {
+    let x: Float
+    let y: Float
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+The hash value is kept in `hashValue` property.
+```swift
+P(x: 2.0, y: 3.0).hashValue
+```
+
+Declaration of the `Hashable` protocol is following:
+```swift
+public protocol Hashable : Equatable {
+    ...
+}
+```
+The Hashable protocol inherits from the Equatable protocol, so you must also satisfy that protocol’s requirements.
+
+The compiler **automatically synthesizes** your custom type’s Hashable and requirements when you declare Hashable conformance in the type’s original declaration and your **type meets** these criteria:
+
+- For a `struct`, all its stored properties must conform to Hashable or Equatable.
+
+- For an `enum`, all its associated values must conform to Hashable. (An enum without associated values has Hashable conformance even without the declaration.)
+
+```swift
+struct User {
+    let name: String
+    let age: Int
+}
+
+extension User: Hashable {   
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(age)
+    }
+}
+```
+> Note!!! There is no need to explicitly conform to the `Equatable`. It is **automatically synthesized**.
+
+In the following example we need to conform to the `Equatable`, because the `Email` stored property does not conform to `Hashable` or `Equatable`.
+
+```swift
+struct Email {
+    let value: String
+}
+struct GmailUser {
+    let name: String //Note the Email does not conform to Hashable or Equatable
+    let email: Email
+}
+
+//Compilation error: Type 'GmailUser' does not conform to protocol 'Equatable'
+extension GmailUser: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+}
+```
+
+To fix the error we need to implicitly make `GmailUser` conform to the `Equatable` protocol:
+```swift
+extension GmailUser: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        (lhs.email.value == rhs.email.value) && (lhs.name == rhs.name)
+    }
+}
+```
+
+, or make Email conforming to the `Hashable`:
+
+```swift
+extension Email: Hashable {   
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(value)
+    }
+}
+```
+
+or `Equatable`:
+```swift
+extension Email: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.value == rhs.value
+    }
+}
+```
+
+> Note!!! Hash values are recomputed every time an app is launch.
+<img src="preview.gif">
--- SwiftUITipsAndTricksTests/SwiftUITipsAndTricksTests.swift
+++ SwiftUITipsAndTricksTests/SwiftUITipsAndTricksTests.swift
@@ -8,29 +8,96 @@
 import XCTest
 @testable import SwiftUITipsAndTricks
 
-final class SwiftUITipsAndTricksTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+struct User {
+    let name: String
+    let age: Int
+}
+
+extension User: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(age)
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+final class UserHashingTests: XCTestCase {
+    
+    func test_set_holds_different_instances_of_users_despite_their_hash_is_equal() {
+        let user1 = User(name: "Rob", age: 40)
+        let user2 = User(name: "John", age: 40)
+        var users: Set<User> = []
+        users.insert(user1)
+        users.insert(user2)
+        
+        XCTAssertEqual(user1.hashValue, user2.hashValue)
+        XCTAssertNotEqual(user1, user2)
+        XCTAssertEqual(users.count, 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_conforming_to_hashable_autosynthetizes_equatable() {
+        
+        XCTAssertEqual(User(name: "Rob", age: 40), User(name: "Rob", age: 40))
+        XCTAssertNotEqual(User(name: "John", age: 40), User(name: "Rob", age: 40))
+        XCTAssertNotEqual(User(name: "Rob", age: 1), User(name: "Rob", age: 10000000))
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+struct Email {
+    let value: String
+}
+
+struct GmailUser {
+    let name: String
+    let email: Email
+}
+
+extension GmailUser: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
     }
+}
 
+//To satisfy the compiler we need to make GmailUser conforming to Equatable
+/*
+extension GmailUser: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        (lhs.email.value == rhs.email.value) && (lhs.name == rhs.name)
+    }
+}
+*/
+
+
+//or make email property conforming to Equatable
+extension Email: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.value == rhs.value
+    }
+}
+
+//or make email property conforming to Hashable
+extension Email: Hashable {
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(value)
+    }
+}
+
+
+
+final class GmailUserHashingTests: XCTestCase {
+   
+    func test_set_holds_only_one_instance_of_equal_objects() throws {
+        var emailUsers: Set<GmailUser> = []
+        emailUsers.insert(GmailUser(name: "Rob", email: Email(value: "rob@gmail.com")))
+        emailUsers.insert(GmailUser(name: "Rob", email: Email(value: "rob@gmail.com")))
+        XCTAssertEqual(emailUsers.count, 1)
+    }
+    
+    func test_set_holds_multiple_instances_of_different_objects() throws {
+        var emailUsers: Set<GmailUser> = []
+        emailUsers.insert(GmailUser(name: "Rob", email: Email(value: "rob@gmail.com")))
+        emailUsers.insert(GmailUser(name: "Rob", email: Email(value: "")))
+        XCTAssertEqual(emailUsers.count, 2)
+    }
 }
