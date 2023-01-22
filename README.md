efd9b8844b467e53e39901374ab95644a161a54d
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-01-22 12:48:31 +0100

    Load/fetch from remote url using combine and url session

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-## Text example
+## Show loaded content on list using `URLSession.shared.dataTaskPublisher`
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+    private func loadCountries() -> AnyPublisher<[Country], Error> {
+        guard let url = URL(string: "https://restcountries.com/v2/all") else {
+            let error: Error = URLError(.badURL)
+            return Fail(outputType: [Country].self, failure: error).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Country].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
     }
-}
 ```
 
-<img src="preview.png" width="40%" >
+
+<img src="preview.gif" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,57 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+struct Country: Identifiable, Decodable {
+    var id: String {
+        return name
+    }
+    let name: String
+}
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var countries: [Country] = []
+    private var cancellable: Cancellable?
+    
+    func load() {
+        cancellable = loadCountries()
+            .sink { result in
+                print(result)
+            } receiveValue: { countries in
+                self.countries = countries
+            }
+    }
+    
+    private func loadCountries() -> AnyPublisher<[Country], Error> {
+        guard let url = URL(string: "https://restcountries.com/v2/all") else {
+            let error: Error = URLError(.badURL)
+            return Fail(outputType: [Country].self, failure: error).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Country].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
 
 struct ContentView: View {
-
+    
+    @ObservedObject private var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        if viewModel.countries.isEmpty {
+            ProgressView()
+                .onAppear { viewModel.load() }
+        } else {
+            List {
+                ForEach(viewModel.countries) { country in
+                    Text(country.name)
+                }
+            }
+        }
     }
 }
 
