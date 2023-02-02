53d7b366e964b3398c9088e392aa5ab4653554ea
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-02 08:04:10 +0100

    Show fallthrough example

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
-## Text example
+## Fallthrough jump to the next case in switch
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+enum Days {
+    case mon
+    case tue
+    case wed
+    case thu
+    case fri
+    case sat
+    case sun
+    
+    var name: String {
+        switch self {
+        case .mon:
+            return "Monday"
+        ...
+        }
+    }
+    
+    var fullDescription: String {
+        var description = ""
+        switch self {
+        case .mon, .tue, .wed, .thu, .fri:
+            description = "Working day: "
+            fallthrough
+        default:
+            description += self.name
+        }
+        return description
     }
 }
 ```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,54 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+enum Days {
+    case mon
+    case tue
+    case wed
+    case thu
+    case fri
+    case sat
+    case sun
+    
+    var name: String {
+        switch self {
+        case .mon:
+            return "Monday"
+        case .tue:
+            return "Tuesday"
+        case .wed:
+            return "Wednesday"
+        case .thu:
+            return "Thusrday"
+        case .fri:
+            return "Friday"
+        case .sat:
+            return "Saturday"
+        case .sun:
+            return "Sunday"
+        }
+    }
+    
+    var fullDescription: String {
+        var description = ""
+        switch self {
+        case .mon, .tue, .wed, .thu, .fri:
+            description = "Working day: "
+            fallthrough
+        default:
+            description += self.name
+        }
+        return description
+    }
+}
 
+struct ContentView: View {
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text(Days.sun.fullDescription)
+            Text(Days.mon.fullDescription)
+        }
     }
 }
 
