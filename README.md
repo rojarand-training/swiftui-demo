92eee3c11b398b33d1ea97ff8807d0d9ce7f1b38
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-06 14:36:43 +0100

    Show how to decode date provided in custom format

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-## Text example
+## [Decoding dates provided in custom format](https://stackoverflow.com/questions/44682626/swifts-jsondecoder-with-multiple-date-formats-in-a-json-string)
 
+There are a few ways to decode a date provided in a custom format
+
+1. Using decoding strategy: `.formatted(formatter)`
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
-}
+let decoder = JSONDecoder()
+let formatter = DateFormatter()
+formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+decoder.dateDecodingStrategy = .formatted(formatter) //!!!!!!!!!!
+let myStruct = try! decoder.decode(MyStruct.self, from: json)
 ```
 
-<img src="preview.png" width="40%" >
+2. Using decoding strategy: `.custom()`
+```swift
+let decoder = JSONDecoder()
+let formatter = DateFormatter()
+formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+decoder.dateDecodingStrategy = .custom({ dcdr in
+    let strDate = try! dcdr.singleValueContainer().decode(String.self)
+    let formatter = DateFormatter()
+    formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+    return formatter.date(from: strDate)!
+})
+let myStruct = try! decoder.decode(MyStruct.self, from: json)
+```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct MyStruct: Codable {
+    let str: String
+    let date: Date
+}
 
+let json = """
+{
+"str": "Hello",
+"date":"2023-02-06 13:12:59"
+}
+""".data(using: .utf8)!
+
+struct ContentView: View {
+    @State var date: String = ""
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Dates: \(date)")
+            
+            Button("Parse using 'dateDecodingStrategy = .formatted(formatter)'") {
+                let decoder = JSONDecoder()
+                let formatter = DateFormatter()
+                formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+                decoder.dateDecodingStrategy = .formatted(formatter)
+                let myStruct = try! decoder.decode(MyStruct.self, from: json)
+                date = "\(formatter.string(from: myStruct.date))"
+            }
+            
+            Button("Parse using 'dateDecodingStrategy = .custom()'") {
+                let decoder = JSONDecoder()
+                let formatter = DateFormatter()
+                formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+                decoder.dateDecodingStrategy = .custom({ dcdr in
+                    let strDate = try! dcdr.singleValueContainer().decode(String.self)
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+                    return formatter.date(from: strDate)!
+                })
+                let myStruct = try! decoder.decode(MyStruct.self, from: json)
+                date = "\(formatter.string(from: myStruct.date))"
+            }
+        }
+    
     }
 }
 
