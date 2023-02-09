d41daadbebc646334f336917b39cd4d393bb182c
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-09 08:52:22 +0100

    Show preferences key in action

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
-## Text example
+## [Preference Key example](https://swiftwithmajid.com/2020/01/15/the-magic-of-view-preferences-in-swiftui/)
+
+
+SwiftUI has the environment concept which we can use to pass data down into a view hierarchy. But sometimes we need to **pass data up** from child view to the parent view, and this is **where preferences shine**.
 
 ```swift
-struct ContentView: View {
+struct BackgroundColorPreferenceKey: PreferenceKey {
+    static var defaultValue: Color { .clear }
+    static func reduce(value: inout Self.Value, nextValue: () -> Self.Value) {
+        value = nextValue()
+    }
+}
 
+struct BackgroundView<Content: View>: View {
+
+    @State var bgColor = BackgroundColorPreferenceKey.defaultValue
+    let content: () -> Content
+    init(content: @escaping () -> Content) {
+        self.content = content
+    }
+    
     var body: some View {
-        Text("Hello World")
+        content()
+            .background(bgColor)
+            .onPreferenceChange(BackgroundColorPreferenceKey.self) { color in
+                bgColor = color
+            }
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct BackgroundColorPreferenceKey: PreferenceKey {
+    static var defaultValue: Color { .clear }
+    static func reduce(value: inout Self.Value, nextValue: () -> Self.Value) {
+        value = nextValue()
+    }
+}
 
+struct BackgroundView<Content: View>: View {
+
+    @State var bgColor = BackgroundColorPreferenceKey.defaultValue
+    let content: () -> Content
+    init(content: @escaping () -> Content) {
+        self.content = content
+    }
+    
     var body: some View {
-        Text("Hello World")
+        content()
+            .background(bgColor)
+            .onPreferenceChange(BackgroundColorPreferenceKey.self) { color in
+                bgColor = color
+            }
+    }
+}
+
+struct ContentView: View {
+    
+    //NOTE!!! Assinging `defaultValue` causes strange compilation error: "Generic parameter 'Content' could not be inferred"
+    //@State var color: Color = BackgroundColorPreferenceKey.defaultValue
+    @State var color: Color = .clear
+    var body: some View {
+        BackgroundView {
+            VStack {
+                Button("Make bg green") {
+                    color = .green
+                }
+                Button("Make bg red") {
+                    color = .red
+                }
+            }.preference(key: BackgroundColorPreferenceKey.self, value: color)
+        }
     }
 }
 
