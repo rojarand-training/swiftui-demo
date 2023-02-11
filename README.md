024909b108b91e57128b87b1bb66eac7b1c8e2d7
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-11 11:26:03 +0100

    Show how async and nonisloated methods works, mening of struct and class as proprties inside actor

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
-## Text example
+## Nonisolated, private, methods, actors
+
+*Actors* allows to call one method at the time. However we can use **nonisolated** methods to call methods concurently.
+
+Accessing the `actor` properties in a `nonisolated` context requires using `await` keyword.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
+nonisolated func nonIsolatedCalc() async {
+    let _ = await model.name
 }
 ```
 
-<img src="preview.png" width="40%" >
+The **await** blocks the execution of a `nonisolated` method until all `isolated` methods are finished.
+
+Check below. The `nonIsolatedCalc` runs concurently to `isolatedCalc` but it gets suspended at `await model.name`. When `isolatedCalc` is ended, `await model.name` is resumed and `nonIsolatedCalc` continues.
+
+<img src="call-nonisolated-method.gif" width="100%" >
+
+Modifying properties from nonisolated context is impossible. Compilation fihishes with: *Actor-isolated property 'model' can not be mutated from a non-isolated context*
+```swift
+nonisolated func nonIsolatedCalc() async {
+    await model.name = "Hello" //Raises compilation error
+}
+```
+
+Meaning of *value* and *reference* types stored inside the actor.
+
+Accessing reference types inside a `nonisolated` context raises warning shown below:
+<img src="non-sendable-warning.png" width="100%" >
+
+Changing `class` to `struct` resolves the issue.
+
+## Using `async` in actors
+
+In *actors*, *async* methods are also *synchronized*. The *async* methods wait until other `async` or normal methods are finished.
+
+<img src="calling-async.gif" width="100%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,67 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct Model {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
 
+actor ContentViewModel3: ObservableObject {
+    
+    var model = Model(name: "")
+    
+    nonisolated func nonIsolatedCalc() async {
+        print("||| nonIsolatedCalc - Begin, access to internal props is granted when isolated methods are ended")
+        let _ = await model.name
+        print("||| nonIsolatedCalc - End")
+    }
+    
+    func isolatedCalc() {
+        print("---- Begin of isolatedCalc - Main Thread: \(Thread.isMainThread ? "Yes": "No")")
+        innerIsolatedCalc()
+        print("---- End of isolatedCalc - Main Thread: \(Thread.isMainThread ? "Yes": "No")")
+    }
+    
+    private func innerIsolatedCalc() {
+        print("--------- Hello from innerIsolatedCalc - Main Thread: \(Thread.isMainThread ? "Yes": "No")")
+        Thread.sleep(forTimeInterval: 2)
+    }
+    
+    func isolatedAsyncCalc() async {
+
+        print("---- Begin of isolatedAsyncCalc, access to internal props is granted without awaiting because we are in safe context now")
+        let _ = model.name
+        print("---- End of isolatedAsyncCalc")
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var vm1 = ContentViewModel3()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Hello World")
+            Button("Isolated calc") {
+                Task {
+                    await vm1.isolatedCalc()
+                }
+            }
+            .padding()
+            Button("Isolated async calc") {
+                Task {
+                    await vm1.isolatedAsyncCalc()
+                }
+            }
+            .padding()
+            Button("Non isolated calc") {
+                Task {
+                    await vm1.nonIsolatedCalc()
+                }
+            }
+            .padding()
+        }
     }
 }
 
