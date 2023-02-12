4ae04f750dcde545c6e40401d631e3796c40a7e6
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-12 13:21:32 +0100

    Show how to chain publishers in combine

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
-## Text example
+## Chaining publishers
+- [swiftbysundell](https://www.swiftbysundell.com/articles/connecting-and-merging-combine-publishers-in-swift/)
+- [swiftwithmajid](https://swiftwithmajid.com/2021/05/04/chaining-publishers-with-combine-framework-in-swift/)
+- [peterfriese](https://peterfriese.dev/posts/swiftui-combine-networking-errorhandling/)
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
+func load() {
+    countries = []
+    cancellable = countriesLocalStoragePublisher()
+        //replace error with empty array of countries
+        .replaceError(with: [Country]())
+        .flatMap { countries in
+            //If county list is empty try to load from remote storage
+            if countries.isEmpty {
+                return self.countriesRemoteStoragePublisher()
+            } else {
+                return Just(countries)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            }
+        }
+        .receive(on: RunLoop.main)
+        .sink { error in
+            
+        } receiveValue: { countries in
+            self.countries = countries
+        }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,85 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+struct Country: Identifiable, Decodable {
+    let name: String
+    var id: String { name }
+}
+
+typealias Countries = [Country]
+
+enum LoadError: Error {
+    case noData
+}
+
+final class ContentViewModel: ObservableObject {
+    
+    @Published var countries: Countries = []
+    
+    private var cancellable: AnyCancellable?
+    
+    func load() {
+        countries = []
+        cancellable = countriesLocalStoragePublisher()
+            .replaceError(with: [Country]())
+            .flatMap { countries in
+                if countries.isEmpty {
+                    return self.countriesRemoteStoragePublisher()
+                } else {
+                    return Just(countries)
+                        .setFailureType(to: Error.self)
+                        .eraseToAnyPublisher()
+                }
+            }
+            .receive(on: RunLoop.main)
+            .sink { error in
+                
+            } receiveValue: { countries in
+                self.countries = countries
+            }
+    }
+    
+    private func countriesLocalStoragePublisher() -> AnyPublisher<Countries, Error> {
+        if Int.random(in: 1...10)%2 == 0 {
+            return Fail(error: LoadError.noData).eraseToAnyPublisher()
+        } else {
+            return Just([Country(name: "Poland"),
+                         Country(name: "Germany"),
+                         Country(name: "UK")])
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+        }
+    }
+    
+    private func countriesRemoteStoragePublisher() -> AnyPublisher<Countries, Error> {
+        guard let url = URL(string: "https://restcountries.com/v2/all") else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: Countries.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
 
 struct ContentView: View {
-
+    @ObservedObject var vm = ContentViewModel()
     var body: some View {
-        Text("Hello World")
+        VStack {
+            if vm.countries.isEmpty {
+                ProgressView()
+                    .onAppear(perform: vm.load)
+            } else {
+                Button("Reload") {
+                    vm.load()
+                }
+                List(vm.countries) { country in
+                    Text(country.name)
+                }
+            }
+        }
     }
 }
 
