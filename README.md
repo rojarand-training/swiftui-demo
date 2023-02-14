bd3cdb9adf37c5bed864228084b8991eb9d31715
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-14 10:19:47 +0100

    Show how to sort using mixed technics

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
-## Text example
+## [Sorting collections](https://www.swiftbysundell.com/articles/sorting-swift-collections/)
+
+By conforming a building block to `Comparable`, the `sorted` method becomes available. 
 
 ```swift
-struct ContentView: View {
+extension Country: Comparable {
+    static func < (lhs: Country, rhs: Country) -> Bool {
+        lhs.name < rhs.name
+    }
+}
 
-    var body: some View {
-        Text("Hello World")
+extension ContentViewModel {
+    func sortByDefault() {
+        countries = countries.sorted()
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+To use `SortDescriptor` the building block has to expose itself to *@objc* runtime.
+
+```swift
+
+class Country: NSObject {
+    let name: String
+    @objc let areaInSquareKm: Double
+    let citiesOver1M: Int
+}
+...
+countries = countries.sorted(using: SortDescriptor(\Country.areaInSquareKm, order: .forward))
+```
+
+<img src="preview.gif" width="60%">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,141 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+extension Sequence {
+    func sorted<Property: Comparable>(by keyPath: KeyPath<Element, Property>,
+                                      comparator: (Property, Property) -> Bool = (<)) -> [Element] {
+        self.sorted { element1, element2 in
+            comparator(element1[keyPath: keyPath], element2[keyPath: keyPath])
+        }
+    }
+}
 
+class Country: NSObject {
+    let name: String
+    @objc let areaInSquareKm: Double
+    let citiesOver1M: Int
+    
+    init(name: String,
+         areaInSquareKm: Double,
+         citiesOver1M: Int) {
+        self.name = name
+        self.areaInSquareKm = areaInSquareKm
+        self.citiesOver1M = citiesOver1M
+    }
+}
+
+final class ContentViewModel: ObservableObject {
+    @Published var countries: [Country] = [
+        Country(name: "Poland", areaInSquareKm: 312000, citiesOver1M: 1),
+        Country(name: "Lithuania", areaInSquareKm: 65000, citiesOver1M: 0),
+        Country(name: "Germany", areaInSquareKm: 357000, citiesOver1M: 4),
+        Country(name: "France", areaInSquareKm: 551000, citiesOver1M: 1),
+        Country(name: "Russia", areaInSquareKm: 17100000, citiesOver1M: 16),
+        Country(name: "Czechia", areaInSquareKm: 78000, citiesOver1M: 1),
+    ]
+}
+
+struct CountrySorting {
+    typealias Strategy = (Country, Country) -> Bool
+    let strategy: Strategy
+}
+
+extension CountrySorting {
+    static var citiesOver1M: CountrySorting {
+        CountrySorting(strategy: { country1, country2 in
+            country1.citiesOver1M < country2.citiesOver1M
+        })
+    }
+}
+
+extension ContentViewModel {
+    func sort<Property: Comparable>(by property: KeyPath<Country, Property>,
+                                    comparator: (Property, Property) -> Bool = (<)) {
+        countries = countries.sorted(by: property, comparator: comparator)
+    }
+    
+    func sort(by sorting: CountrySorting) {
+        countries = countries.sorted(by: sorting.strategy)
+    }
+    
+    func sortByAreaDesc() {
+        countries = countries.sorted(using: SortDescriptor(\Country.areaInSquareKm, order: .reverse))
+    }
+    
+    func sortByAreaAsc() {
+        countries = countries.sorted(using: SortDescriptor(\Country.areaInSquareKm, order: .forward))
+    }
+}
+
+//-------
+
+extension Country: Comparable {
+    static func < (lhs: Country, rhs: Country) -> Bool {
+        lhs.name < rhs.name
+    }
+}
+
+extension ContentViewModel {
+    func sortByDefault() {
+        countries = countries.sorted()
+    }
+}
+
+//-------
+
+struct ContentView: View {
+    @ObservedObject var vm = ContentViewModel()
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack(alignment: .center) {
+                Spacer()
+                Button {
+                    vm.sort(by: \.name, comparator: <)
+                } label: {
+                    VStack {
+                        Image(systemName: "cellularbars")
+                        Text("Name ASC").font(.caption2)
+                    }
+                }
+                Button {
+                    vm.sort(by: \.name, comparator: >)
+                } label: {
+                    VStack {
+                        Image(systemName: "cellularbars")
+                        Text("Name DESC").font(.caption2)
+                    }
+                }
+                Button {
+                    vm.sortByAreaAsc()
+                } label: {
+                    VStack {
+                        Image(systemName: "cellularbars")
+                        Text("Area ASC").font(.caption2)
+                    }
+                }
+                Button {
+                    vm.sortByAreaDesc()
+                } label: {
+                    VStack {
+                        Image(systemName: "cellularbars")
+                        Text("Area DESC").font(.caption2)
+                    }
+                }
+                Button {
+                    vm.sort(by: .citiesOver1M)
+                } label: {
+                    VStack {
+                        Image(systemName: "cellularbars")
+                        Text("Big cities").font(.caption2)
+                    }
+                }
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            List(vm.countries, id: \.name) { country in
+                Text(country.name)
+            }
+        }
     }
 }
 
