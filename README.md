4143a92be35670589cc8062f8ce6d95701d99a4a
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-14 08:39:27 +0100

    Show how to combine publishers, delay publication, validate form

--- README.md
+++ README.md
@@ -1,12 +1,67 @@
-## Text example
+## Combining publishers
 
-```swift
+*Published* proprties have `Publisher` capabilities. They can publish values. For instance `@Published var userName: String = ""` property bound to a *TextField* publishes a new string value every time an user type something.
+
+```
 struct ContentView: View {
+    ...
+    TextField("Username", text: $vm.userName)
+    ...
+}
+...
 
-    var body: some View {
-        Text("Hello World")
+final class ContentViewModel: ObservableObject {
+    
+    @Published var userName: String = ""
+
+    var userNamePublisher: AnyPublisher<Bool, Never> {
+        $userName
+            //A publisher that publishes events only after a specified time elapses.
+            .debounce(for: 0.1, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map {
+                print("userName \($0)")
+                return $0.count > 5
+            }
+            .eraseToAnyPublisher()
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+Publishers can be **combined** to produce merged result:
+```swift
+extension ContentViewModel {
+    
+    var isValidPublisher: AnyPublisher<Bool, Never> {
+        Publishers.CombineLatest(userNamePublisher, passwordPublisher)
+            .map { userNameValid, passwordValid in
+                userNameValid && passwordValid
+            }.eraseToAnyPublisher()
+    }
+    
+    var userNamePublisher: AnyPublisher<Bool, Never> {
+        $userName
+            //A publisher that publishes events only after a specified time elapses.
+            .debounce(for: 0.1, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map {
+                print("userName \($0)")
+                return $0.count > 5
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    var passwordPublisher: AnyPublisher<Bool, Never> {
+        $password
+            .debounce(for: 0.1, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map {
+                print("password \($0)")
+                return $0.count > 5
+            }
+            .eraseToAnyPublisher()
+    }
+}
+```
+
+<img src="preview.gif" width="60%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,111 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+final class ContentViewModel: ObservableObject {
+    
+    @Published var userName: String = ""
+    @Published var userNameError: String = ""
+    @Published var password: String = ""
+    @Published var passwordError: String = ""
+    @Published var isValid: Bool = false
+   
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        userNamePublisher
+            .receive(on: RunLoop.main)
+            .map { userNameValid in
+                userNameValid ? "" : "User name has to be at least 5 chars long"
+            }
+            .assign(to: \.userNameError, on: self)
+            .store(in: &cancellables)
+        
+        
+        passwordPublisher
+            .receive(on: RunLoop.main)
+            .map { passwordValid in
+                passwordValid ? "" : "The password has to be at least 5 chars long"
+            }
+            .assign(to: \.passwordError, on: self)
+            .store(in: &cancellables)
+        
+        isValidPublisher
+            .map { valid in
+                print("Valid: \(valid)")
+                return valid
+            }
+            .assign(to: \.isValid, on: self)
+            .store(in: &cancellables)
+    }
+}
+
+extension ContentViewModel {
+    
+    var isValidPublisher: AnyPublisher<Bool, Never> {
+        Publishers.CombineLatest(userNamePublisher, passwordPublisher)
+            .map { userNameValid, passwordValid in
+                userNameValid && passwordValid
+            }.eraseToAnyPublisher()
+    }
+    
+    var userNamePublisher: AnyPublisher<Bool, Never> {
+        $userName
+            //A publisher that publishes events only after a specified time elapses.
+            .debounce(for: 0.1, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map {
+                print("userName \($0)")
+                return $0.count > 5
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    var passwordPublisher: AnyPublisher<Bool, Never> {
+        $password
+            .debounce(for: 0.1, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map {
+                print("password \($0)")
+                return $0.count > 5
+            }
+            .eraseToAnyPublisher()
+    }
+}
 
 struct ContentView: View {
 
+    @ObservedObject var vm = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            TextField("Username", text: $vm.userName)
+            Text(vm.userNameError)
+                .foregroundColor(.red)
+                .font(.caption2)
+                .padding(.bottom, 10)
+            
+            SecureField("Password", text: $vm.password)
+                
+            Text(vm.passwordError)
+                .foregroundColor(.red)
+                .font(.caption2)
+                .padding(.bottom, 10)
+            
+            Button {
+                print("Registering")
+            } label: {
+                Text("Register")
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .background(vm.isValid ? .green : .red)
+            .cornerRadius(8)
+            .disabled(!vm.isValid)
+        }
+        .padding(.leading, 20)
+        .padding(.trailing, 20)
     }
 }
 
