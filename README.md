dbbf73d91fe34e9712761fcc29dd3ec9a437aec5
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-16 19:18:42 +0100

    Show example od decoding variant model without polymorphism

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-## Text example
+## [Variant model decoding example](https://www.swiftbysundell.com/articles/handling-model-variants-in-swift/)
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+ init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let decodableItems = try container.decode(DecodableMultimediaItems.self)
+    
+    videos = decodableItems.items
+        .filter { $0.type == .video }
+        .map { item in
+            Video(name: item.name, type: item.type, length: item.length!)
+    }
+    
+    images = decodableItems.items
+        .filter { $0.type == .image }
+        .map { item in
+            Image(name: item.name, type: item.type, size: item.size!)
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.png" width="80%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,119 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+let response = """
+{ "items": [
+    {"name": "video1", "type": "video", "length": "1:25:00"},
+    {"name": "video2", "type": "video", "length": "0:05:10"},
+    {"name": "image1", "type": "image", "size": 2048},
+    {"name": "image2", "type": "image", "size": 4096}
+]}
+""".data(using: .utf8)!
 
+enum MultimediaType: String, Decodable {
+    case video
+    case image
+}
+
+protocol MultimediaItem {
+    var name: String { get }
+    var type: MultimediaType { get }
+}
+
+struct Video {
+    let name: String
+    let type: MultimediaType
+    let length: String
+}
+
+struct Image {
+    let name: String
+    let type: MultimediaType
+    let size: Int
+
+}
+
+struct MultimediaItems {
+    let videos: [Video]
+    let images: [Image]
+}
+
+struct UnexpectedNilProperty: Error {
+    let propertyName: String
+}
+
+func unwrap<T>(property: T?, propertyName: String) throws -> T {
+    if let property {
+        return property
+    } else {
+        throw UnexpectedNilProperty(propertyName: propertyName)
+    }
+}
+
+extension MultimediaItems: Decodable {
+    
+    struct DecodableMultimediaItem: Decodable {
+        let name: String
+        let type: MultimediaType
+        let length: String?
+        let size: Int?
+    }
+    
+    struct DecodableMultimediaItems: Decodable {
+        let items: [DecodableMultimediaItem]
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let decodableItems = try container.decode(DecodableMultimediaItems.self)
+        
+        videos = try decodableItems.items
+            .filter { $0.type == .video }
+            .map { item in
+                Video(name: item.name,
+                      type: item.type,
+                      length: try unwrap(property: item.length, propertyName: "lenght"))
+            }
+        
+        images = try decodableItems.items
+            .filter { $0.type == .image }
+            .map { item in
+                Image(name: item.name,
+                      type: item.type,
+                      size: try unwrap(property: item.size, propertyName: "size"))
+            }
+    }
+}
+
+struct ContentView: View {
+    
+    private let items: MultimediaItems
+    init() {
+        items = Self.buildItems()
+    }
+    
     var body: some View {
-        Text("Hello World")
+        
+        List {
+            Section("Videos") {
+                ForEach(items.videos, id: \.name) { video in
+                    Text("Name: \(video.name), lenght: \(video.length)")
+                }
+            }
+            Section("Images") {
+                ForEach(items.images, id: \.name) { image in
+                    Text("Name: \(image.name), size: \(image.size)")
+                }
+            }
+        }
+    }
+    
+    private static func buildItems() -> MultimediaItems {
+        do {
+            return try JSONDecoder().decode(MultimediaItems.self, from: response)
+        } catch {
+            return MultimediaItems(videos: [], images: [])
+        }
     }
 }
 
