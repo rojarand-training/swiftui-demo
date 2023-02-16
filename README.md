d3401e9b87c5c920286fc01ce4e94651a449dbb4
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-16 19:18:42 +0100

    Show example od decoding variant model

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-## Text example
+## [Variant model decoding example](https://www.swiftbysundell.com/articles/handling-model-variants-in-swift/)
+
+It is worth to mention that `Decoder` class has two methods to obtain a `container`.
+1. `container(keyedBy: Key.Type)`
+ The above method allows to decode step by step by passing key describing name of a property to decode.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+extension Item: Decodable {
+    enum Properties: CodingKey {
+        case type
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Properties.self)
+        let type = try container.decode(ItemVariantType.self, forKey: .type)
+        ...
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+Should be used when a *swift* model is differtent than its encoded representation.
+
+2. `singleValueContainer()`
+The above method returns container which is able to decode an entire `Decodable` instance.
+
+```swift
+let container = try decoder.singleValueContainer()
+let item = try container.decode(Item.self)
+```
+
+Should be used when a *swift* model is the same as its encoded representation.
+
+<img src="preview.png" width="80%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,114 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+let response = """
+[
+    {"name": "video1", "type": "videoType", "length": "1:25:00"},
+    {"name": "video2", "type": "videoType", "length": "0:05:10"},
+    {"name": "image1", "type": "imageType", "size": 2048},
+    {"name": "image2", "type": "imageType", "size": 4096}
+]
+""".data(using: .utf8)!
 
+enum ItemVariantType: String, Decodable {
+    case videoType
+    case imageType
+}
+
+protocol ItemVariant: Decodable {
+    var name: String { get }
+    var type: ItemVariantType { get }
+}
+
+struct Video: ItemVariant {
+    let name: String
+    let length: String
+    var type: ItemVariantType {
+        .videoType
+    }
+}
+
+struct Image: ItemVariant {
+    let name: String
+    let size: Int
+    var type: ItemVariantType {
+        .imageType
+    }
+}
+
+enum Item {
+    case video(Video)
+    case image(Image)
+}
+
+extension Item: Decodable {
+    enum Properties: CodingKey {
+        case type
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Properties.self)
+        let type = try container.decode(ItemVariantType.self, forKey: .type)
+        switch type {
+        case .imageType:
+            self = .image(try Image(from: decoder))
+        case .videoType:
+            self = .video(try Video(from: decoder))
+        }
+    }
+}
+
+extension Array where Element==Item {
+    var videos: [Video] {
+        self.compactMap { item in
+            if case Item.video(let video) = item {
+                return video
+            } else {
+                return nil
+            }
+        }
+    }
+    
+    var images: [Image] {
+        self.compactMap { item in
+            if case Item.image(let image) = item {
+                return image
+            } else {
+                return nil
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    let items: [Item]
+    
+    init() {
+        self.items = Self.loadItems()
+    }
+    
     var body: some View {
-        Text("Hello World")
+        List {
+            Section("Videos") {
+                ForEach(items.videos, id: \.name) { video in
+                    Text("Name: \(video.name), length: \(video.length)")
+                }
+            }
+            Section("Images") {
+                ForEach(items.images, id: \.name) { image in
+                    Text("Name: \(image.name), size: \(image.size)")
+                }
+            }
+        }
+    }
+    
+    private static func loadItems() -> [Item] {
+        do {
+            return try JSONDecoder().decode([Item].self, from: response)
+        } catch  {
+            return []
+        }
     }
 }
 
