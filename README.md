c7dd130becedc292006adda71b4bd81096c7068c
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-23 08:40:51 +0100

    Show how scan operator works

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
-## Text example
+## [Combine Scan](https://paigeshin1991.medium.com/swift-combine-scan-method-the-most-useful-operator-fae10bc8debf)
+
+The `scan` operator of combine acts as Swift's `reduce`. It aggregates history of updates.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+final class ContentViewModel: ObservableObject {
+    
+    @Published var history: String = ""
+    var currentText: String = ""
+    
+    private var subject = CurrentValueSubject<String, Error>("")
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        cancellable = subject
+            .debounce(for: 1, scheduler: DispatchQueue.main)
+            .filter({ string in
+                string.count > 1
+            })
+            .scan("") { nextPartialResult, newString in
+            nextPartialResult + "\n "+newString
+        }.sink(receiveCompletion: { completion in
+            
+        }, receiveValue: { string in
+            self.history = string
+        })
+    }
+    
+    func updateCurrentText(newText: String) {
+        currentText = newText
+        subject.send(newText)
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,59 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+final class ContentViewModel: ObservableObject {
+    
+    @Published var history: String = ""
+    var currentText: String = ""
+    
+    private var subject = CurrentValueSubject<String, Error>("")
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        cancellable = subject
+            .debounce(for: 1, scheduler: DispatchQueue.main)
+            .filter({ string in
+                string.count > 1
+            })
+            .scan("") { nextPartialResult, newString in
+            nextPartialResult + "\n "+newString
+        }.sink(receiveCompletion: { completion in
+            
+        }, receiveValue: { string in
+            self.history = string
+        })
+    }
+    
+    func updateCurrentText(newText: String) {
+        currentText = newText
+        subject.send(newText)
+    }
+}
 
 struct ContentView: View {
 
+    @ObservedObject private var vm: ContentViewModel
+    
+    init() {
+        vm = ContentViewModel()
+    }
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            TextField("Type something", text: text)
+            Text(vm.history)
+        }
+    }
+    
+    var text: Binding<String> {
+        Binding {
+            self.vm.currentText
+        } set: { newTxt in
+            self.vm.updateCurrentText(newText: newTxt)
+        }
+
     }
 }
 
