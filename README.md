2d728b21ce7cf2fd0bc249600c805074021ec7c1
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-24 08:18:11 +0100

    Show example of @dynamicMemberLookup, KeyPath and generic observable

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
-## Text example
+## [Dynamic Member Lookup](https://www.swiftbysundell.com/tips/building-an-observable-type-for-swiftui-views/)
+
+[hackingwithswift](https://www.hackingwithswift.com/articles/55/how-to-use-dynamic-member-lookup-in-swift)
+
+
+The `@dynamicMemberLookup` feature let you write Swift in the way you would expect from languages like JavaScript or Python keeping is strong typing safety.
+
+The `@dynamicMemberLookup` property wrapper requires the `subscript` to be implemented in the wrapped class.
+```console
+@dynamicMemberLookup attribute requires 'Observalble' to have a 'subscript(dynamicMember:)' method that accepts either 'ExpressibleByStringLiteral' or a key path
+```
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+@dynamicMemberLookup
+final class Observalble<Value>: ObservableObject {
+    
+    @Published var value: Value
+    private var cacellable: AnyCancellable?
+    
+    init<P: Publisher>(value: Value, publisher: P) where P.Output == Value, P.Failure == Never {
+        self.value = value
+        self.cacellable = publisher.assign(to: \.value, on: self)
+    }
+    
+    subscript<T>(dynamicMember keyPath: KeyPath<Value, T>) -> T {
+        value[keyPath: keyPath]
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+@dynamicMemberLookup
+final class Observalble<Value>: ObservableObject {
+    
+    @Published var value: Value
+    private var cacellable: AnyCancellable?
+    
+    init<P: Publisher>(value: Value, publisher: P) where P.Output == Value, P.Failure == Never {
+        self.value = value
+        self.cacellable = publisher.assign(to: \.value, on: self)
+    }
+    
+    subscript<T>(dynamicMember keyPath: KeyPath<Value, T>) -> T {
+        value[keyPath: keyPath]
+    }
+}
+
+struct Article {
+    let title: String
+    let author: String
+}
 
 struct ContentView: View {
-
+    
+    @ObservedObject private var articleVM: Observalble<Article>
+    private let authorSubject = PassthroughSubject<Article, Never>()
+    init() {
+        let article = Article(title: "Hello World", author: "Author")
+        articleVM = Observalble(value: article, publisher: authorSubject)
+    }
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Author: \(articleVM.author)")
+            Text("Title: \(articleVM.title)")
+            
+            Button("Author 1") {
+                let article = Article(title: "Hello World1", author: "Author1")
+                authorSubject.send(article)
+            }
+            Button("Author 2") {
+                let article = Article(title: "Hello World2", author: "Author2")
+                authorSubject.send(article)
+            }
+            Button("Author 3") {
+                let article = Article(title: "Hello World3", author: "Author3")
+                authorSubject.send(article)
+            }
+        }
     }
 }
 
