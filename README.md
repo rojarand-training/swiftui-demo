b63caf0a8770a98b4cb9d174e5d7ecad032e0e40
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-25 10:55:20 +0100

    Explain diff between throttle and debounce

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-## Text example
+## [Combine Throttle vs Debounce](https://blog.canopas.com/swift-11-useful-combine-operators-you-need-to-know-499cfdce0cd5)
 
+The `throttle` operator waits for the specified interval repeatedly, and at the end of each interval, it will emit either the first or the last of the values depending on what is passed for its `latest` parameter.
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
-}
+.throttle(for: 1, scheduler: DispatchQueue.main, latest: [true|false])
 ```
 
-<img src="preview.png" width="40%" >
+The `debounce` operator waits for a specific time span from the emission of the **last** value before emitting the **last** value received. In other words it waits until a publisher before is iddle for the given time, then emits the last value.
+
+
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,98 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+
+final class ContentViewModel: ObservableObject {
+    @Published var throttleValue: Int = 0
+    @Published var debounceValue: Int = 0
+    
+    private let throttleSubject = PassthroughSubject<Int, Error>()
+    private let debounceSubject = PassthroughSubject<Int, Error>()
+    private var cancellables: Set<AnyCancellable> = []
+    init() {
+        //1000ms
+        throttleSubject
+            .throttle(for: 1, scheduler: DispatchQueue.main, latest: true)
+            .sink { _ in
+            } receiveValue: { value in
+                self.throttleValue = value
+            }.store(in: &cancellables)
+        
+        debounceSubject
+            .debounce(for: 0.5, scheduler: DispatchQueue.main)
+            .sink { _ in
+            } receiveValue: { value in
+                NSLog("Debounce receive: \(value)")
+                self.debounceValue = value
+            }.store(in: &cancellables)
+    }
+    
+    func runThrottling() {
+        Task {
+            var index = 0
+            let stream = AsyncThrowingStream<Int,Error> {
+                guard index < 10 else { return nil }
+                index += 1
+                try await Task.sleep(nanoseconds: 450_000_000)//450ms
+                return index
+            }
+            for try await i in stream {
+                throttleSubject.send(i)
+            }
+        }
+    }
+    
+    func runDebouncing() {
+        Task {
+            var index = 0
+            //2 will be received, because next will be send after 600 ms
+            let delays = [(1, 200), (2, 200), (3, 600), (4, 200), (5,200), (6,200), (7,200)]
+            let stream = AsyncThrowingStream<Int,Error> {
+                guard index < delays.count else { return nil }
+                let delay = delays[index]
+                index += 1
+                
+                NSLog("Will send: \(delay.0), after: \(delay.1) ms")
+                try await Task.sleep(nanoseconds: UInt64(delay.1*1_000_000))//450ms
+                return delay.0
+            }
+            
+            for try await i in stream {
+                NSLog("Debounce send: \(i)")
+                debounceSubject.send(i)
+            }
+        }
+    }
+}
 
 struct ContentView: View {
 
+    @ObservedObject private var vm = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            VStack {
+                Text("Throttle emits value with constant delay")
+                Text("Throttle value: \(vm.throttleValue)")
+                Button("Run throttling") {
+                    vm.runThrottling()
+                }
+            }
+            .padding()
+            .background(.yellow)
+            
+            VStack {
+                Text("Debounce waits until there is nothing received, then emits LAST element")
+                Text("Debounce value: \(vm.debounceValue)")
+                Button("Run debounce") {
+                    vm.runDebouncing()
+                }
+            }
+            .padding()
+            .background(.red)
+        }
     }
 }
 
