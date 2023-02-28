e44ae95e1a0bacde09f0a98f47239f6728aecf1a
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-02-28 09:09:28 +0100

    Show example how and when to use 'switchToLatest'

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-## Text example
+## [switchToLatest](https://www.polpiella.dev/the-power-of-the-switchtolatest-operator)
+
+- [Other resource](https://www.createwithswift.com/reference-combine-switchtolatest/)
+
+A common use case would be that multiple requests for asynchronous work may be issued, but only the **latest request is really needed**. In case if the upstream logic is expensive, then your app performance will be badly affected without cancelling unwanted subscriptions.
+
+For example, requests could be aimed at loading images from a remote source. A user might tap on a button to load the image but may decide to move on and tap on another button to load another image before the first request is even completed. In this case, subscriptions in Combine can be configured with a `.switchToLatest()` operator to receive a new publisher from the upstream publisher and **cancel its previous subscription**.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
+init() {
+    cancellables = subject
+        .map { divider in
+            (1...100).filter { value in
+                (value % divider) == 0
+            }.publisher
+                .collect()
+                .delay(for: 1, scheduler: DispatchQueue.main) //simulate delay
+        }
+        .switchToLatest()
+        .map({ res in
+            "\(res)"
+        })
+        .assign(to: \.result, on: self)
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,92 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+
+final class ContentViewModel: ObservableObject {
+   
+    @Published var result: String = ""
+    private let subject = PassthroughSubject<Int, Never>()
+    private var cancellables: AnyCancellable?
+    
+    init() {
+        cancellables = subject
+            .map { divider in
+                (1...100).filter { value in
+                    (value % divider) == 0
+                }.publisher
+                    .collect()
+                    .delay(for: 1, scheduler: DispatchQueue.main)
+            }
+            .switchToLatest()
+            .map({ res in
+                "\(res)"
+            })
+            .assign(to: \.result, on: self)
+    }
+    
+    func dividable(by divider: Int) {
+        subject.send(divider)
+    }
+}
 
 struct ContentView: View {
-
+    
+    @ObservedObject private var vm = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Show numbers dividible by: ")
+            HStack {
+                Button {
+                    vm.dividable(by: 2)
+                } label: {
+                    Text("2")
+                        .roundedCornersButton()
+                }
+                
+                Button {
+                    vm.dividable(by: 3)
+                } label: {
+                    Text("3")
+                        .roundedCornersButton()
+                }
+                
+                Button {
+                    vm.dividable(by: 5)
+                } label: {
+                    Text("5")
+                        .roundedCornersButton()
+                }
+                
+                Button {
+                    vm.dividable(by: 7)
+                } label: {
+                    Text("7")
+                        .roundedCornersButton()
+                }
+            }
+            Text("Result: \(vm.result)")
+        }
+    }
+}
+
+extension View {
+    func roundedCornersButton() -> some View {
+        return self.modifier(RoundedCornerButton())
+    }
+}
+
+struct RoundedCornerButton: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .padding()
+            .background(.green)
+            .cornerRadius(12)
+            .foregroundColor(.white)
     }
 }
 
