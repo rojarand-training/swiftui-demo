f5bcdcccdd916fba0908c4d4403aba5f19354504
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-02 10:09:40 +0100

    Show how to debug causes of a view recreation

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-## Text example
+## [Debugging what causes body recreation](https://www.hackingwithswift.com/quick-start/swiftui/how-to-find-which-data-change-is-causing-a-swiftui-view-to-update)
+
+Use `Self._printChanges()` to print source of body recreation.
 
 ```swift
-struct ContentView: View {
-
+struct InnerView: View {
+    
     var body: some View {
-        Text("Hello World")
+        composeBody()
+    }
+    
+    @ViewBuilder func composeBody() -> some View {
+        let _ = Self._printChanges()
+        Text("I'm inner view")
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
 
-    var body: some View {
-        Text("Hello World")
+final class ContentViewModel: ObservableObject {
+    @Published var value1: Int = 0
+    @Published var value2: Int = 0
+    
+    
+    func incValue1() {
+        value1 += 1
     }
+    
+    func incValue2() {
+        value2 += 1
+    }
+}
+
+struct InnerView: View {
+    
+    var body: some View {
+        composeBody()
+    }
+    
+    @ViewBuilder func composeBody() -> some View {
+        let _ = Self._printChanges()
+        Text("I'm inner view")
+    }
+}
+
+struct ContentView: View {
+    
+    @Environment(\.presentationMode) var presentation
+    @ObservedObject private var viewModel = ContentViewModel()
+    
+    var body: some View {
+        composeBody()
+    }
+    
+    @ViewBuilder func composeBody() -> some View {
+        let _ = Self._printChanges()
+        NavigationView {
+            VStack {
+                NavigationLink("Go to next view") {
+                    InnerView()
+                }
+                HStack {
+                    Button("Inc value1") {
+                        viewModel.incValue1()
+                    }
+                    Text("Value1: \(viewModel.value1)")
+                }
+                HStack {
+                    Button("Inc value2") {
+                        viewModel.incValue2()
+                    }
+                    Text("Value2: \(viewModel.value2)")
+                }
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
