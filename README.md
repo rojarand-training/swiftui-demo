ee0930cd41fb92a209be7a431c58334ce63a47fd
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-04 09:59:36 +0100

    Show example how to create onboarding

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-## Text example
+## [Onboarding](https://dev.to/domanovdev/swiftui-onboarding-view-1165)
 
 ```swift
 struct ContentView: View {
 
+    @State private var selection = 1
+    
     var body: some View {
-        Text("Hello World")
+        
+        TabView(selection: $selection) {
+            Text("Tab1")
+                .zoomInOnAppear()
+                .tag(0)
+            
+            Text("Tab2")
+                .zoomInOnAppear()
+                .tag(1)
+            
+            Text("Tab3")
+                .zoomInOnAppear()
+                .tag(2)
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" width="60%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -10,8 +10,45 @@ import CoreData
 
 struct ContentView: View {
 
+    @State private var selection = 1
+    
     var body: some View {
-        Text("Hello World")
+        
+        TabView(selection: $selection) {
+            Text("Tab1")
+                .zoomInOnAppear()
+                .tag(0)
+            
+            Text("Tab2")
+                .zoomInOnAppear()
+                .tag(1)
+            
+            Text("Tab3")
+                .zoomInOnAppear()
+                .tag(2)
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
+}
+
+extension View {
+    func zoomInOnAppear() -> some View {
+        self.modifier(ZoomIn())
+    }
+}
+
+struct ZoomIn: ViewModifier {
+    @State var isAnimating = false
+    func body(content: Content) -> some View {
+        content
+            .scaleEffect(isAnimating ? 3.0 : 0.9)
+            .onAppear {
+            isAnimating = false
+            withAnimation(.easeOut(duration: 1.2)) {
+                isAnimating = true
+            }
+        }
     }
 }
 
