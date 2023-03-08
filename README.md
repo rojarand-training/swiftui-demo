7706b01aa4d1d532af4378262b483da06c22f5b3
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-08 07:55:47 +0100

    Show how to create UIViewRepresentable generic wrapper

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-## Text example
+## [Wrapping UIView](https://www.swiftbysundell.com/tips/inline-wrapping-of-uikit-or-appkit-views-within-swiftui/)
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+struct UIViewWrapper<V: UIView>: UIViewRepresentable {
+    
+    private let viewMaking: () -> V
+    private let viewUpdating: (V, Context) -> Void
+    
+    init(viewMaking: @escaping () -> V,
+         viewUpdating: @escaping (V, Context) -> Void) {
+        
+        self.viewMaking = viewMaking
+        self.viewUpdating = viewUpdating
+    }
+    
+    func makeUIView(context: Context) -> V {
+        viewMaking()
+    }
+    
+    func updateUIView(_ uiView: V, context: Context) {
+        viewUpdating(uiView, context)
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 import CoreData
 
+struct UIViewWrapper<V: UIView>: UIViewRepresentable {
+    
+    private let viewMaking: () -> V
+    private let viewUpdating: (V, Context) -> Void
+    
+    init(viewMaking: @escaping () -> V,
+         viewUpdating: @escaping (V, Context) -> Void) {
+        
+        self.viewMaking = viewMaking
+        self.viewUpdating = viewUpdating
+    }
+    
+    func makeUIView(context: Context) -> V {
+        viewMaking()
+    }
+    
+    func updateUIView(_ uiView: V, context: Context) {
+        viewUpdating(uiView, context)
+    }
+}
+
 struct ContentView: View {
 
+    @State private var isLoading = false
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            UIViewWrapper {
+                UIActivityIndicatorView()
+            } viewUpdating: { indicator, _ in
+                if isLoading {
+                    indicator.startAnimating()
+                } else {
+                    indicator.stopAnimating()
+                }
+            }
+            Toggle(isLoading ? "Off" : "On", isOn: $isLoading)
+                .toggleStyle(.button)
+        }
     }
 }
 
