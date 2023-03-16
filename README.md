3078dd38b8d9512429e90b8cdc9214e2308547b5
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-16 13:18:20 +0100

    Add example of existential (any) and opaque (some) types

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 ## Text example
 
+https://www.avanderlee.com/swift/some-opaque-types/
+
+https://swiftsenpai.com/swift/dynamic-dispatch-with-generic-protocols/
+
+https://swiftsenpai.com/swift/understanding-some-and-any/#:~:text=the%20%E2%80%9Cany%E2%80%9D%20Keyword-,The%20any%20keyword%20was%20introduced%20in%20Swift%205.6.,you%20failed%20to%20do%20so.
+
+Opaque types allow you to describe the expected return type without defining a concrete type. 
+
+```swift
+var body: some View { ... }
+```
+
+The compiler can see the following return types:
+```swift
+var body: VStack { ... }
+
+// or:
+
+var body: Text { ... }
+```
+
 ```swift
 struct ContentView: View {
+   
+    let anyViews: [any View] = [Button("Tap me") {}, Text("Hello") ]
+    //Different types can be mixed in 'any' collection
+    
+    /* Compilation error: "Type of expression is ambiguous without more context"
+    let someViews: [some View] = [Button("Tap me") {}, Text("Hello") ]
+    Different types can not be mixed in 'some' collection
+    */
+    let someViews: [some View] = [Text("Hello"), Text("World")]
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            AnyView(anyViews[0])
+            someViews[0]
+        }
     }
 }
 ```
-
-<img src="preview.png" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,139 @@
 import SwiftUI
 import CoreData
 
+protocol Fuel {
+    associatedtype FuelType where FuelType==Self
+    static func purchase() -> FuelType
+}
+
+struct Gasoline: Fuel {
+    static func purchase() -> Gasoline { Gasoline() }
+}
+
+struct Diesel: Fuel {
+    static func purchase() -> Diesel { Diesel() }
+}
+
+protocol Vehicle: Equatable {
+    associatedtype FuelType: Fuel
+    func fillTank(with fuel: FuelType)
+    func startEngine()
+}
+
+struct Car: Vehicle {
+    let number: Int
+    init(number: Int = 0) {
+        self.number = number
+    }
+    func fillTank(with fuel: Gasoline) {}
+    func startEngine() {}
+}
+
+struct Bus: Vehicle {
+    func fillTank(with fuel: Diesel) {}
+    func startEngine() {}
+}
+
+
+extension Car {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.number == rhs.number
+    }
+}
+
+extension Bus {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        true
+    }
+}
+
+
+/*
+let vehicles: [Vehicle]//Use of protocol 'Vehicle' as a type must be written 'any Vehicle'
+*/
+let differentVehicles: [any Vehicle] = [Car(), Bus()]
+
+/*
+let theSameVehicles: [some Vehicle] = [Car(), Bus()] //Type of expression is ambiguous without more context
+*/
+
+let theSameVehicles: [some Vehicle] = [Car(), Car()] //Type of expression is ambiguous without more context
+
+func startAllEngines(for vehicles: [any Vehicle]) {
+    vehicles.forEach { vehicle in
+        vehicle.startEngine()
+    }
+}
+
+func createAnyVehicle() -> any Vehicle {
+    if (Int.random(in: 1...10) % 2) == 0 {
+        return Car()
+    } else {
+        return Bus()
+    }
+}
+
+let anyVehicle1 = createAnyVehicle()
+let anyVehicle2 = createAnyVehicle()
+/*
+Does not compile:
+ let anyVehiclesEqual = anyVehicle1 == anyVehicle2
+*/
+
+func createSomeVehicle() -> some Vehicle {
+    print("Called: createSomeVehicle() -> some Vehicle")
+    if (Int.random(in: 1...10) % 2) == 0 {
+        return Car(number: 0)
+    } else {
+        return Car(number: 1)
+    }
+}
+
+/*
+func createSomeVehicle() -> Car {
+    print("Called: createSomeVehicle() -> Car")
+    if (Int.random(in: 1...10) % 2) == 0 {
+        return Car(number: 0)
+    } else {
+        return Car(number: 1)
+    }
+}*/
+
+//func fillAllTanks(for vehicles: [any Vehicle]) //error
+func fillAllTanks(for vehicles: [some Vehicle]) {
+    vehicles.forEach { vehicle in
+        let fuel = type(of: vehicle).FuelType.purchase()
+        vehicle.fillTank(with: fuel)
+    }
+}
+
+//func fillAllTanks<V:Vehicle>(for vehicles: [V]) { //Invalid redeclaration of 'fillAllTanks(for:)'
+func fillAllTanks2<V:Vehicle>(for vehicles: [V]) {
+    vehicles.forEach { vehicle in
+        let fuel = type(of: vehicle).FuelType.purchase()
+        vehicle.fillTank(with: fuel)
+    }
+}
+
+final class ContentViewModel: ObservableObject {
+}
+
 struct ContentView: View {
+   
+    let anyViews: [any View] = [Button("Tap me") {}, Text("Hello") ]
+    //Different types can be mixed in 'any' collection
+    
+    /* Compilation error: "Type of expression is ambiguous without more context"
+    let someViews: [some View] = [Button("Tap me") {}, Text("Hello") ]
+    Different types can not be mixed in 'some' collection
+    */
+    let someViews: [some View] = [Text("Hello"), Text("World")]
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            AnyView(anyViews[0])
+            someViews[0]
+        }
     }
 }
 
