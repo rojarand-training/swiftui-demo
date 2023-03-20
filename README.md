fa36d308cfdde3e3ffe2d8802e29d58639444010
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-20 08:18:37 +0100

    Show how applying layoutPriority affects layout

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-## Text example
+## [Advanced layout positioniong and sizing](https://www.swiftbysundell.com/articles/swiftui-layout-system-guide-part-3/)
+
+For andvanced laupout management you can use: `layoutPriority` and frame `frame` modifiers.
 
 ```swift
-struct ContentView: View {
-
+struct Page4: View {
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            NavigationLink("Go to page 5") {
+                Page5()
+            }
+            EventHeader(title: "Image placeholder layoutPriority=-1 + minHeight=100")
+            Text(test1)
+                .font(.title3)
+            ImagePlaceholder()
+                .layoutPriority(-1)
+                .frame(minHeight: 100)
+            Text(test2)
+            Spacer()
+            EventInfoList()
+        }.padding(5)
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -11,17 +11,105 @@ import CoreData
 struct ContentView: View {
 
     var body: some View {
+        NavigationStack {
+            Page1()
+        }
+    }
+}
+
+let test1 = (1...20).reduce("") { res, value in res + "Bla bla " }
+let test2 = (1...60).reduce("") { res, value in res + "Hello World, " }
+
+struct Page1: View {
+    var body: some View {
         VStack(alignment: .leading) {
-            EventHeader()
-            Text((1...20).reduce("") { res, value in res + "Bla bla " })
+            NavigationLink("Go to page 2") {
+                Page2()
+            }
+            EventHeader(title: "Layout without priorities or min/max sizes")
+            Text(test1)
                 .font(.title3)
             ImagePlaceholder()
-            Text((1...50).reduce("") { res, value in res + "Hello World, " })
+            Text(test2)
             Spacer()
             EventInfoList()
         }.padding(5)
     }
 }
+
+struct Page2: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            NavigationLink("Go to page 3") {
+                Page3()
+            }
+            EventHeader(title: "Text2 layout layoutPriority=1")
+            Text(test1)
+                .font(.title3)
+            ImagePlaceholder()
+            Text(test2).layoutPriority(1)
+            Spacer()
+            EventInfoList()
+        }.padding(5)
+    }
+}
+
+struct Page3: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            NavigationLink("Go to page 4") {
+                Page4()
+            }
+            EventHeader(title: "Image placeholder layoutPriority=-1")
+            Text(test1)
+                .font(.title3)
+            ImagePlaceholder().layoutPriority(-1)
+            Text(test2)
+            Spacer()
+            EventInfoList()
+        }.padding(5)
+    }
+}
+
+
+struct Page4: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            NavigationLink("Go to page 5") {
+                Page5()
+            }
+            EventHeader(title: "Image placeholder layoutPriority=-1 + minHeight=100")
+            Text(test1)
+                .font(.title3)
+            ImagePlaceholder()
+                .layoutPriority(-1)
+                .frame(minHeight: 100)
+            Text(test2)
+            Spacer()
+            EventInfoList()
+        }.padding(5)
+    }
+}
+
+struct Page5: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            NavigationLink("Go to page 6") {
+                Page2()
+            }
+            EventHeader(title: "Image placeholder layoutPriority=-1 + minHeight=100 + text2 layoutPriority=1")
+            Text(test1)
+                .font(.title3)
+            ImagePlaceholder()
+                .layoutPriority(-1)
+                .frame(minHeight: 100)
+            Text(test2)
+            Spacer()
+            EventInfoList().layoutPriority(1)
+        }.padding(5)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -67,12 +155,15 @@ struct EventInfo: View {
 }
 
 struct EventHeader: View {
-   
+    let title: String
+    init(title: String = "Title title") {
+        self.title = title
+    }
     var body: some View {
         HStack {
             CalendarBadge()
             VStack(alignment: .leading) {
-                Text("Title title")
+                Text(title)
                     .font(.title3)
                 Text("Subtitle subtitle")
                     .font(.caption2)
