27a8bcce6d5926dccf40e999be370fef9adce495
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-21 08:47:12 +0100

    Show how to adapt closure based api to combine

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
-## Text example
+## [Swift futures](https://www.swiftbysundell.com/articles/using-combine-futures-and-subjects/)
+
+Takeaways:
+- **Futures are publishers.**
+- Futures are greate for adapting closure based code to reactive with Combine.
 
 ```swift
-struct ContentView: View {
+func load() {
+    cancellable = loadArticle()//Note it returns Future and is used as an publisher
+        .replaceError(with: Article(text: "Could not load"))
+        .receive(on: DispatchQueue.main)
+        .sink(receiveValue: { article in
+            self.loadState = .loaded(article)
+        })
+}
 
-    var body: some View {
-        Text("Hello World")
+private func loadArticle() -> Future<Article, Error> {
+    Future { promise in
+        self.loadArticleGCD { article in
+            if let article = article {
+                promise(.success(article))
+            } else {
+                promise(.failure(Error.notAvailable))
+            }
+        }
+    }
+}
+
+private func loadArticleGCD(handler: @escaping (Article?) -> Void) {
+    DispatchQueue.global().asyncAfter(deadline: .now()+2) {
+        if Int.random(in: 1...5)%2 == 0 {
+            handler(Article(text: "Hello world"))
+        } else {
+            handler(nil)
+        }
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" width="60%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,78 @@
 //
 
 import SwiftUI
-import CoreData
+import Combine
+
+struct Article {
+    let text: String
+}
+
+enum LoadState {
+    case notLoaded
+    case loaded(Article)
+}
+
+final class ContentViewModel: ObservableObject {
+    @Published var loadState: LoadState = .notLoaded
+    
+    private var cancellable: AnyCancellable?
+    
+    enum Error: Swift.Error {
+        case notAvailable
+    }
+    
+    func reload() {
+        loadState = .notLoaded
+    }
+    
+    func load() {
+        cancellable = loadArticle()
+            .replaceError(with: Article(text: "Could not load"))
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { article in
+                self.loadState = .loaded(article)
+            })
+    }
+    
+    private func loadArticle() -> Future<Article, Error> {
+        Future { promise in
+            self.loadArticleGCD { article in
+                if let article = article {
+                    promise(.success(article))
+                } else {
+                    promise(.failure(Error.notAvailable))
+                }
+            }
+        }
+    }
+    
+    private func loadArticleGCD(handler: @escaping (Article?) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now()+2) {
+            if Int.random(in: 1...5)%2 == 0 {
+                handler(Article(text: "Hello world"))
+            } else {
+                handler(nil)
+            }
+        }
+    }
+}
 
 struct ContentView: View {
 
+    @StateObject private var vm = ContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        switch vm.loadState {
+        case .notLoaded:
+            Text("Loading ...").onAppear(perform: vm.load)
+        case .loaded(let article):
+            VStack {
+                Text("Article text: \(article.text)")
+                Button("Reload") {
+                    vm.reload()
+                }
+            }
+        }
     }
 }
 
