f323fc091e4e2e901516ae50c82c83ea06709226
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-28 09:31:21 +0200

    Show how to slice a collection

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-## Text example
+## [Slicing collections](https://www.swiftbysundell.com/articles/slicing-swift-collections/)
 
-```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
-}
-```
-
-<img src="preview.png" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 import CoreData
 
+extension BidirectionalCollection {
+    
+    func page(withIndex index: Int, size: Int) -> SubSequence {
+        dropFirst(index*size).prefix(size)
+    }
+}
+
+struct Numbers {
+    let content = (1...10).map { $0 }
+    
+    func first(_ count: Int) -> ArraySlice<Int> {
+        content.prefix(count)
+        
+    }
+    func last(_ count: Int) -> ArraySlice<Int> {
+        content.suffix(count)
+    }
+    
+    func first(where conditionMet:(Int) -> Bool) -> ArraySlice<Int> {
+        content.prefix(while: conditionMet)
+    }
+    
+    func page(withIndex index: Int, size: Int) -> ArraySlice<Int> {
+        content.page(withIndex: index, size: size)
+    }
+}
+
+extension ArraySlice {
+    var content: String {
+        let elements = reduce("") { partialResult, element in
+            if !partialResult.isEmpty {
+                return partialResult + ", \(element)"
+            } else {
+                return "\(element)"
+            }
+        }
+        return "[\(elements)]"
+    }
+}
+
 struct ContentView: View {
 
+    let numbers = Numbers()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("First 100: \(numbers.first(100).content)")
+            Text("Last 3: \(numbers.last(3).content)")
+            Text("Less than 5: \(numbers.first{ $0 <= 5 }.content)")
+            Text("Page with index:2, size:3 \(numbers.page(withIndex:2, size: 3).content)")
+        }
     }
 }
 
