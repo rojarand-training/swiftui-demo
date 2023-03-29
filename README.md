5a63d4e4d6110895345d269b9eee749ae3043346
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-03-29 09:52:09 +0200

    Show how to store collections of references weakly

--- README.md
+++ README.md
@@ -1,12 +1,53 @@
-## Text example
+## [Store collections of references weakly](https://www.swiftbysundell.com/articles/combining-value-and-reference-types-in-swift/)
 
+Weak refenrence can be applied to reference type only.
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
+Weak<Object: AnyObject> {
+    weak var object: Object?
 }
 ```
+Unfortunatelly it leads to the following erorr:
 
-<img src="preview.png" width="40%" >
+Error: 'Weak' requires that 'MagicNumberObserver' be a class type when applying struct.
+
+Thanks to applying the "provider" closure we are able to fix the error.
+
+> Note!!! For structures `object` it will always be `nil`
+
+```swift
+struct Weak<Object> {
+    var object: Object? {
+        provider()
+    }
+    
+    private let provider: () -> Object?
+    
+    init(_ object: Object) {
+        let reference = object as AnyObject
+        provider = { [weak reference] in
+            reference as? Object
+        }
+    }
+}
+
+struct NonLeakyMagicNumberGenerator {
+    
+    private var observers = [Weak<MagicNumberObserver>]()
+    
+    func generate() {
+        let number = drawMagicNumber()
+        notifyNumberChanged(number)
+    }
+    
+    mutating func addObserver(_ observer: MagicNumberObserver) {
+        observers.append(Weak(observer))
+    }
+    
+    private func notifyNumberChanged(_ number: Int) {
+        observers.forEach { weakObserver in
+            weakObserver.object?.onMagicNumber(number)
+        }
+    }
+}
+
+```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,140 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+func drawMagicNumber() -> Int {
+    Int.random(in: 1...100)
+}
 
+protocol MagicNumberObserver {
+    func onMagicNumber(_ number: Int)
+}
+
+struct MagicNumberLogger: MagicNumberObserver {
+    
+    func onMagicNumber(_ number: Int) {
+        NSLog("Magic number: \(number)")
+    }
+}
+
+struct LeakyMagicNumberGenerator {
+    
+    private var observers = [MagicNumberObserver]()
+    
+    func generate() {
+        let number = drawMagicNumber()
+        notifyNumberChanged(number)
+    }
+    
+    mutating func addObserver(_ observer: MagicNumberObserver) {
+        observers.append(observer)
+    }
+    
+    private func notifyNumberChanged(_ number: Int) {
+        observers.forEach { observer in
+            observer.onMagicNumber(number)
+        }
+    }
+}
+
+struct Weak<Object> {
+    var object: Object? {
+        provider()
+    }
+    
+    private let provider: () -> Object?
+    
+    init(_ object: Object) {
+        let reference = object as AnyObject
+        provider = { [weak reference] in
+            reference as? Object
+        }
+    }
+}
+
+struct NonLeakyMagicNumberGenerator {
+    
+    private var observers = [Weak<MagicNumberObserver>]()
+    
+    func generate() {
+        let number = drawMagicNumber()
+        notifyNumberChanged(number)
+    }
+    
+    mutating func addObserver(_ observer: MagicNumberObserver) {
+        observers.append(Weak(observer))
+    }
+    
+    private func notifyNumberChanged(_ number: Int) {
+        observers.forEach { weakObserver in
+            let isNil = (weakObserver.object != nil) ? "non nil" : "NIL"
+            NSLog("Observer: \(isNil)")
+            weakObserver.object?.onMagicNumber(number)
+        }
+    }
+}
+
+
+
+final class MagicContentViewModel: ObservableObject, MagicNumberObserver {
+    
+    @Published private (set) var number: Int = 0
+    private var generator = NonLeakyMagicNumberGenerator()
+//    private var generator = LeakyMagicNumberGenerator()
+    
+    private var logger = MagicNumberLogger()
+    
+    init() {
+        generator.addObserver(self)
+        generator.addObserver(logger)
+    }
+    
+    deinit {
+        print("MagicContentViewModel deinit")
+    }
+    
+    func reset() {
+        
+    }
+    
+    func generate() {
+        generator.generate()
+    }
+    
+    func onMagicNumber(_ number: Int) {
+        self.number = number
+    }
+}
+
+
+struct MagicContentView: View {
+    @ObservedObject private var vm = MagicContentViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Magic number: \(vm.number)")
+            Button("Generate magic number") {
+                vm.generate()
+            }
+            Button("Reset") {
+                vm.reset()
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var isShown = false
+    var body: some View {
+        
+        VStack {
+            Toggle("Show magic content", isOn: $isShown)
+            if isShown {
+                MagicContentView()
+            } else {
+                Text("Magic content is hidden")
+            }
+        }
     }
 }
 
