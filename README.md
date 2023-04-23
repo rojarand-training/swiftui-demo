fe1cfec159706b3c3cbec24d727aa97785fa8154
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-04-23 10:13:55 +0200

    Show how to use images and other views as a backgrounds

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-## Text example
+## [How to use images and other views as a backgrounds](https://www.hackingwithswift.com/quick-start/swiftui/how-to-use-images-and-other-views-as-a-backgrounds)
+
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
-}
+/*
+    By default background views automatically take up as much space as they need to be fully visible, but if you want you can have them be clipped to the size of their parent view using the clipped()
+*/
+Text("Hello World")
+    .font(.system(size: 50))
+    .foregroundColor(.green)
+    .padding(20)
+    .background(
+        Circle()
+            .fill(.red)
+            .frame(width: 150, height: 150)
+    )
+    .clipped()
 ```
 
 <img src="preview.png" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -11,7 +11,36 @@ import CoreData
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Hello World")
+                .font(.system(size: 50))
+                .foregroundColor(.green)
+                .padding(20)
+                .background(
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 150, height: 150)
+                )
+            
+            Spacer()
+                .frame(height: 50)
+            
+            /*
+             By default background views automatically take up as much space as they need to be fully visible, but if you want you can have them be clipped to the size of their parent view using the clipped()
+            */
+            Text("Hello World")
+                .font(.system(size: 50))
+                .foregroundColor(.green)
+                .padding(20)
+                .background(
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 150, height: 150)
+                )
+                .clipped()
+            
+            
+        }
     }
 }
 
