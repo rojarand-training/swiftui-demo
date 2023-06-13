c92074bb872edcff9a50e954c9a12093bc3de838
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-06-13 07:36:08 +0200

    Show how to make views equal height and width

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
-## Text example
+## [How to make two views the same width or height](https://www.hackingwithswift.com/quick-start/swiftui/how-to-make-two-views-the-same-width-or-height)
+
+On iOS, the key is to give each view you want to size an infinite maximum height or width, which will automatically make it stretch to fill all the available space. You then apply `fixedSize()` to the container they are in, which tells SwiftUI those **views should only take up the space they need**.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
+VStack {
+    Button("Sign in") {}
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.green)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+    Button("Create a new account") {}
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
 }
+.fixedSize(horizontal: true, vertical: false)
 ```
 
 <img src="preview.png" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -9,9 +9,69 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                Text("This is a short text 1")
+                    .padding()
+                    .background(.green)
+                    .foregroundColor(.white)
+                Text("This is a very very very very  very very long string 1")
+                    .padding()
+                    .background(.red)
+                    .foregroundColor(.white)
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            //.frame(maxHeight: 100)
+            
+            HStack {
+                Text("This is a short string 2")
+                    .padding()
+                    .background(.green)
+                    .foregroundColor(.white)
+                Text("This is a very very very very  very very long string 2")
+                    .padding()
+                    .background(.red)
+                    .foregroundColor(.white)
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(maxHeight: 100)
+            
+            VStack {
+                Button("Sign in") {}
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.green)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                Button("Create a new account") {}
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.red)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            }
+            .fixedSize(horizontal: true, vertical: false)
+            
+            VStack {
+                Button("Log in") { }
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.red)
+                    .clipShape(Capsule())
+
+                Button("Reset Password") { }
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.red)
+                    .clipShape(Capsule())
+            }
+            .fixedSize(horizontal: true, vertical: false)
+            
+        }
     }
 }
 
