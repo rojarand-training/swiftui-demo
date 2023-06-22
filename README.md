f9e5d99b735b7dc98ce775d64bda96fcc73632f6
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-06-22 07:41:19 +0200

    How to create a custom layout using the Layout protocol

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-## Text example
+## [How to create a custom layout using the Layout protocol](https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-custom-layout-using-the-layout-protocol)
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+struct RadialLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        guard !subviews.isEmpty else { return }
+        
+        let angle = Angle(degrees: 360.0/CGFloat(subviews.count)).radians
+        let r = min(bounds.width, bounds.height)/2
+        subviews.enumerated().forEach { index, subview in
+            let angleDelta = CGFloat(index)*angle
+            var dx = cos(angleDelta) * r
+            let dy = sin(angleDelta) * r
+            subview.place(at: CGPoint(x: bounds.midX+dx, y: bounds.midY+dy), anchor: .center, proposal: proposal)
+        }
+        
     }
 }
 ```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 import CoreData
 
+
+struct RadialLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        guard !subviews.isEmpty else { return }
+        
+        let angle = Angle(degrees: 360.0/CGFloat(subviews.count)).radians
+        let r = min(bounds.width, bounds.height)/2
+        subviews.enumerated().forEach { index, subview in
+            let angleDelta = CGFloat(index)*angle
+            var dx = cos(angleDelta) * r * 0.95
+            let dy = sin(angleDelta) * r * 0.95
+            subview.place(at: CGPoint(x: bounds.midX+dx, y: bounds.midY+dy), anchor: .center, proposal: proposal)
+        }
+        
+    }
+}
+
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello World")
+        RadialLayout {
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("4")
+            Text("5")
+            Text("6")
+            Text("7")
+            Text("8")
+        }
     }
 }
 
