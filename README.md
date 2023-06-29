a5ae8bc8707640ea7b0ffb095182f19406431b9a
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-06-29 07:41:25 +0200

    Show how to show a Map view

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-## Text example
+## [How to show a Map view](https://www.hackingwithswift.com/quick-start/swiftui/how-to-show-a-map-view)
 
 ```swift
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
     var body: some View {
-        Text("Hello World")
+        Map(coordinateRegion: $region)
+            .frame(width: 400, height: 300)
     }
 }
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif" width="40%" >
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
-import CoreData
+import MapKit
 
 struct ContentView: View {
 
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var longitude = 0.0
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Map(coordinateRegion: $region)
+                .frame(width: 400, height: 300)
+            Slider(value: $longitude)
+                .onChange(of: longitude) { newValue in
+                    region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: newValue), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+                }
+        }
     }
 }
 
