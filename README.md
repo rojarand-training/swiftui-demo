11eba515ed3ea17c6c27a3be7a1abe8cd5d07254
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-11-04 06:50:21 +0100

    Explain how to access raw unsafe memory layout pointers

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-## Text example
+## Pointers
+
+https://www.kodeco.com/7181017-unsafe-swift-using-pointers-and-interacting-with-c?page=1
+
+https://quickbirdstudios.com/blog/swift-unsafe-raw-bytes-pointers-ios/
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
-    }
-}
+@frozen
+enum MemoryLayout<T>
 ```
+The memory layout of a type, describing its size, stride, and alignment.
 
-<img src="preview.png" width="40%" >
+`static var size: Int`
+
+The contiguous memory footprint of T, in bytes.
+
+`static var alignment: Int`
+
+The default memory alignment of T, in bytes.
+
+`static var stride: Int`
+
+The number of bytes from the start of one instance of `T` to the start of the next when stored in contiguous memory or in an `Array<T>`.
+
+Always use a multiple of a type’s **stride** instead of its size when allocating memory or accounting for the distance between instances in memory. 
+
+##Samples in tests
--- SwiftUITipsAndTricksTests/SwiftUITipsAndTricksTests.swift
+++ SwiftUITipsAndTricksTests/SwiftUITipsAndTricksTests.swift
@@ -18,12 +18,73 @@ final class SwiftUITipsAndTricksTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_changing_bytes_of_primitive() throws {
+        var i32: Int32 = 0x00000000
+        let bytes = withUnsafeMutableBytes(of: &i32) { bytes in
+            bytes[0] = 0xAA
+            return bytes
+        }
+        bytes[1] = 0xBB
+        XCTAssertEqual(0x0000BBAA, i32)
+    }
+    
+    struct SampleStruct {
+      let number: UInt32
+      let flag: Bool
+    }
+    
+    func test_changing_bytes_of_struct() throws {
+        var sampleStruct = SampleStruct(number: 0, flag: false)
+        withUnsafeMutableBytes(of: &sampleStruct) { bytes in
+            bytes[0] = 0xAA
+            bytes[MemoryLayout<SampleStruct>.size-1] = 0x01
+        }
+        XCTAssertEqual(sampleStruct.number, 0x000000AA)
+        XCTAssertEqual(sampleStruct.flag, true)
+    }
+    
+    func test_storing_and_loading_struct_from_raw_bytes() throws {
+        let count = 2
+        let size = MemoryLayout<SampleStruct>.size              // returns 5
+        let alignment = MemoryLayout<SampleStruct>.alignment    // returns 4
+        let stride = MemoryLayout<SampleStruct>.stride          // returns 8
+        
+        let pointer = UnsafeMutableRawPointer.allocate(byteCount: count*stride, alignment: alignment)
+        defer {
+            pointer.deallocate()
+        }
+        
+        let inSS1 = SampleStruct(number: 0xAACC, flag: false)
+        pointer.storeBytes(of: inSS1, as: SampleStruct.self)
+        let outSS1 = pointer.load(as: SampleStruct.self)
+        XCTAssertEqual(inSS1.number, outSS1.number)
+        XCTAssertEqual(inSS1.flag, outSS1.flag)
+        
+        let inSS2 = SampleStruct(number: 123, flag: true)
+        pointer.advanced(by: stride).storeBytes(of: inSS2, as: SampleStruct.self)
+        let outSS2 = pointer.advanced(by: stride).load(as: SampleStruct.self)
+        XCTAssertEqual(inSS2.number, outSS2.number)
+        XCTAssertEqual(inSS2.flag, outSS2.flag)
+    }
+    
+    func test_storing_and_loading_struct_from_typed_pointer() throws {
+        
+        let pointer = UnsafeMutablePointer<SampleStruct>.allocate(capacity: 2)
+        defer {
+            pointer.deallocate()
+        }
+        
+        let inSS1 = SampleStruct(number: 0xAACC, flag: false)
+        pointer.pointee = inSS1
+        let outSS1 = pointer.pointee
+        XCTAssertEqual(inSS1.number, outSS1.number)
+        XCTAssertEqual(inSS1.flag, outSS1.flag)
+        
+        let inSS2 = SampleStruct(number: 123, flag: true)
+        pointer.pointee = inSS2
+        let outSS2 = pointer.pointee
+        XCTAssertEqual(inSS2.number, outSS2.number)
+        XCTAssertEqual(inSS2.flag, outSS2.flag)
     }
 
     func testPerformanceExample() throws {
