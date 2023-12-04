5a8f3b87d60e0a7716f6553a6b9f211628dff783
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-12-04 07:24:53 +0100

    Show how to create content insets/padding

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
-## Text example
-TODO:
-- Review of libraries
-- Swift data
-- Logger
-- BDD with SwiftUI
-- Combine+Countries
-- DatePicker
-- Review of the existing solutions
-- Wrapping text around image
-- Redacted mode for UIKit
+## Content padding
+
+To create padding/insets depending on iOS version use `contentInsets` or `contentEdgeInsets`.
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+func createButton() -> UIButton {
+    if #available(iOS 15.0, *) {
+        var buttonConfiguration = UIButton.Configuration.bordered()
+        buttonConfiguration.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 30, bottom: 50, trailing: 30)
+        return UIButton(configuration: buttonConfiguration, primaryAction: action)
+    } else {
+        let oldStyleButton = UIButton(frame:.zero, primaryAction: action)
+        oldStyleButton.contentEdgeInsets = UIEdgeInsets(top: 20, left: 30, bottom: 50, right: 30)
+        return oldStyleButton
     }
 }
 ```
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,55 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+class MyViewController: UIViewController {
+    
+    private lazy var button: UIButton = {
+        let action = UIAction(title: "Tap me") { [weak self] action in
+            self?.handleButtonTapped()
+        }
+        func createButton() -> UIButton {
+            if #available(iOS 15.0, *) {
+                var buttonConfiguration = UIButton.Configuration.bordered()
+                buttonConfiguration.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 30, bottom: 50, trailing: 30)
+                return UIButton(configuration: buttonConfiguration, primaryAction: action)
+            } else {
+                let oldStyleButton = UIButton(frame:.zero, primaryAction: action)
+                oldStyleButton.contentEdgeInsets = UIEdgeInsets(top: 20, left: 30, bottom: 50, right: 30)
+                return oldStyleButton
+            }
+        }
+        let button = createButton()
+        button.backgroundColor = .blue
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        NSLayoutConstraint.activate(makeLayoutConstraints())
+    }
+    
+    private func makeLayoutConstraints() -> [NSLayoutConstraint] {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor))
+        constraints.append(button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor))
+        return constraints
+    }
+    
+    private func handleButtonTapped() {
+        NSLog("Button tapped")
+    }
+}
 
-    var body: some View {
-        Text("Hello World")
+struct ContentView: UIViewControllerRepresentable {
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        MyViewController()
     }
 }
 
