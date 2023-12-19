c811946e4001c39bcb39f0fa3942b412ffe810b3
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2023-12-19 09:28:32 +0100

    Explain how to animate gradient with layers

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
-## Text example
+## Gradient animation example
+
+Unfortunately `keypath` is a string. Note that the gradient must have frame set. 
 
 ```swift
-struct ContentView: View {
-
-    var body: some View {
-        Text("Hello World")
+override func viewDidLoad() {
+        super.viewDidLoad()
+           
+        let iView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        iView.center = view.center
+        iView.backgroundColor = .yellow
+        view.addSubview(iView)
+        
+        let gradientLayer = CAGradientLayer(layer: iView.layer)
+        gradientLayer.locations = [0, 1] //- 1
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.frame = iView.bounds //- 2
+        iView.layer.insertSublayer(gradientLayer, at: 0)
+        
+        let animation = CABasicAnimation(keyPath: "locations") //- 1
+        animation.fromValue = [0.0, 0.2]
+        animation.toValue = [0.0, 0.9]
+        animation.duration = 1
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        gradientLayer.add(animation, forKey: nil)
     }
-}
 ```
 
-<img src="preview.png" width="40%" >
+<img src="preview.gif">
--- SwiftUITipsAndTricks/ContentView.swift
+++ SwiftUITipsAndTricks/ContentView.swift
@@ -8,10 +8,90 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
 
-    var body: some View {
-        Text("Hello World")
+final class MyVC: UIViewController {
+    
+    private var label : UILabel = {
+        let lab = UILabel()
+        lab.text = "HelloWorld"
+        return lab
+    }()
+    
+    private var labelGradientLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.locations = [0.0, 1.0]
+        layer.zPosition = CGFloat.greatestFiniteMagnitude
+        layer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        layer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        let baseColor: UIColor = .gray
+        let startColor = baseColor.withAlphaComponent(0.3).cgColor
+        let endColor = baseColor.withAlphaComponent(0.9).cgColor
+        layer.colors = [startColor, endColor]
+        
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [-1.0, -0.5, 0.0]
+        animation.toValue = [1.0, 1.5, 2.0]
+        animation.repeatCount = .infinity
+        animation.duration = 1.0
+        layer.add(animation, forKey: nil)
+        return layer
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        NSLayoutConstraint.activate(
+            [
+                label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                label.topAnchor.constraint(equalTo: view.topAnchor)
+            ])
+        
+        label.layer.insertSublayer(labelGradientLayer, at: 0)
+       
+        /////////////////////////////
+        /////////////////////////////
+        /////////////////////////////
+        
+        let iView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        iView.center = view.center
+        iView.backgroundColor = .yellow
+        view.addSubview(iView)
+        
+        let gradientLayer = CAGradientLayer(layer: iView.layer)
+        gradientLayer.locations = [0, 1]
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.frame = iView.bounds
+        iView.layer.insertSublayer(gradientLayer, at: 0)
+        
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [0.0, 0.2]
+        animation.toValue = [0.0, 0.9]
+        animation.duration = 1
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        gradientLayer.add(animation, forKey: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        labelGradientLayer.frame = label.bounds
+        labelGradientLayer.cornerRadius = 5
+    }
+    
+    
+}
+
+struct ContentView: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        MyVC()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
     }
 }
 
